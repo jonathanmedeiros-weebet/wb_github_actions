38588966c7baa3e2c84432bb756ecab15cf30dce
Thiago Faustino <thiagofaustino51@gmail.com>
2018-07-31 11:34:50 -0300

    atualizando angular para versão 6

--- src/app/shared/services/sorteio.service.ts
+++ src/app/shared/services/sorteio.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
 import { HeadersService } from './utils/headers.service';
