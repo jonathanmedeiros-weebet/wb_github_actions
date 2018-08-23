ccca6c858119d1c4e361d02ae2ef28d80d40c074
Lenildo Nogueira <lenildo@lti.net.br>
2018-08-22 22:51:17 -0300

    ajustes header

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,6 +1,6 @@
 
 const _host = 'http://weebet.local';
-const _center = 'http://localhost:3002/v1';
+const _center = 'http://192.168.0.125:3002/v1';
 
 export const config: any = {
     HOST: _host,
@@ -8,7 +8,7 @@ export const config: any = {
     CENTER_URL: _center,
     SPORTS_URL: `${_host}/api/esportes`,
     LOTTERIES_URL: `${_host}/api/loteria`,
-    PRIMARY_COLOR: '#0165b1',
+    PRIMARY_COLOR: '#999',
     SECONDARY_COLOR: '#0165b1',
     LOGO: `${_host}/tema/logo_banca.png`,
     BG: `${_host}/tema/bk.jpg`,
