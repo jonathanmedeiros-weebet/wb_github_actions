ed0c711b5ea41f9c6be2072b963bf4079aedb881
Thiago Faustino <thiagofaustino51@gmail.com>
2018-10-29 19:16:00 -0300

    ajustes

--- src/app/esportes/futebol/default/futebol-default.module.ts
+++ src/app/esportes/futebol/default/futebol-default.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from './../../../shared/shared.module';
 import { FutebolDefaultRoutingModule } from './futebol-default-routing.module';
 import { FutebolDefaultWrapperComponent } from './wrapper/futebol-default-wrapper.component';
-import { FutebolNavigationComponent } from './navigation/futebol-navigation.component';
 import { FutebolFooterComponent } from './footer/futebol-footer.component';
 import { FutebolListagemComponent } from './listagem/futebol-listagem.component';
 import { FutebolJogoComponent } from './jogo/futebol-jogo.component';
@@ -14,7 +13,6 @@ import { FutebolJogoComponent } from './jogo/futebol-jogo.component';
         FutebolDefaultRoutingModule
     ],
     declarations: [
-        FutebolNavigationComponent,
         FutebolFooterComponent,
         FutebolDefaultWrapperComponent,
         FutebolListagemComponent,
