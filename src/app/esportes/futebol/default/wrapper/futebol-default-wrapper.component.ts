29aee224a62eef2447010fd9a62bff79b4bc1f97
Thiago Faustino <thiagofaustino51@gmail.com>
2018-10-30 17:30:28 -0300

    ajustes

--- src/app/esportes/futebol/default/wrapper/futebol-default-wrapper.component.ts
+++ src/app/esportes/futebol/default/wrapper/futebol-default-wrapper.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-futebol-default-wrapper',
-    templateUrl: 'futebol-default-wrapper.component.html',
-    styleUrls: ['futebol-default-wrapper.component.css']
+    templateUrl: 'futebol-default-wrapper.component.html'
 })
 export class FutebolDefaultWrapperComponent {
 
