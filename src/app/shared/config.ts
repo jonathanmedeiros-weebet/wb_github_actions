5d9203fec02c5c719b0bfce8e0ec12285e1a151a
Thiago Faustino <thiagofaustino51@gmail.com>
2018-12-24 11:24:29 -0300

    config

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,8 +1,8 @@
 
-const _host = 'http://demo.wee.bet';
-const _center = 'http://api-center.wee.bet';
-// const _host = 'http://weebet.local';
-// const _center = 'http://localhost:3002';
+// const _host = 'http://demo.wee.bet';
+// const _center = 'http://api-center.wee.bet';
+const _host = 'http://weebet.local';
+const _center = 'http://localhost:3002';
 
 export const config: any = {
     BANCA_NOME: 'DEMO',
@@ -13,5 +13,6 @@ export const config: any = {
     SPORTS_URL: `${_host}/api/esportes`,
     LOTTERIES_URL: `${_host}/api/loteria`,
     LOGO: `${_host}/tema/logo_banca.png`,
-    BG: `${_host}/tema/bk.jpg`
+    BG: `${_host}/tema/bk.jpg`,
+    VERSAO: 1.0
 };
