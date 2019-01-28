f54de713231fbdb82c50f23d74c32e7f1558cd2c
Lenildo <lenildo@lti.net.br>
2019-01-27 23:04:43 -0300

    deploys

--- gulpfile.js
+++ gulpfile.js
@@ -89,6 +89,14 @@ gulp.task('demo.wee.bet', function(done) {
     });
 });
 
+gulp.task('eurosportbet.wee.bet', function(done) {
+    tasks(done, {
+        host: "eurosportbet.wee.bet",
+        banca: "Euro Sport",
+        styles: "--header: #002458; --foreground-header: #b1c5e0; --sidebar-right:#011e46; --foreground-sidebar-right: #fff; --sidebar-left: #011e46; --foreground-sidebar-left: #fff; --highlight: yellow; --foreground-highlight: #aec3d8; --odds: #046eef; --foreground-odds: #fff;"
+    });
+});
+
 gulp.task('maisportes.com', function(done) {
     tasks(done, {
         host: "maisportes.com",
