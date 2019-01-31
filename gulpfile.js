68ba37f719e443dcce0ccc46483ce317f950efda
Lenildo <lenildo@lti.net.br>
2019-01-30 23:58:57 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -72,6 +72,15 @@ gulp.task('betgyn.com', function(done) {
     });
 });
 
+gulp.task('chutecerto.club', function(done) {
+    tasks(done, {
+        host: "chutecerto.club",
+        banca: "Chute Certo",
+        styles: "--header: #002458; --foreground-header: #b1c5e0; --sidebar-right:#011e46; --foreground-sidebar-right: #fff; --sidebar-left: #011e46; --foreground-sidebar-left: #fff; --highlight: yellow; --foreground-highlight: #aec3d8; --odds: #046eef; --foreground-odds: #fff;"
+    });
+});
+
+
 gulp.task('ciabets.wee.bet', function(done) {
     tasks(done, {
         host: "ciabets.wee.bet",
