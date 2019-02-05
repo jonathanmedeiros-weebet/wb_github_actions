7fc204f2a11940a89f8490d2360907cb676cbcf3
Thiago Faustino <thiagofaustino51@gmail.com>
2019-02-05 15:54:25 -0300

    ajustes

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,6 +1,8 @@
 
-const _host = 'http://superbets.bet';
+const _host = 'http://demo.wee.bet';
 const _center = 'http://api-center.wee.bet';
+// const _host = 'http://weebet.local';
+// const _center = 'http://localhost:3002';
 
 export const config: any = {
     BANCA_NOME: 'SUPERBETS',
