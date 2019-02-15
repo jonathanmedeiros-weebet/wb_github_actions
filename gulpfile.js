ab80ca494490eec0dd7a8a3f6937b1bae1064950
Lenildo <lenildo@lti.net.br>
2019-02-15 18:19:38 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -105,6 +105,14 @@ gulp.task('demo.wee.bet', function(done) {
     });
 });
 
+gulp.task('esportivatop.wee.bet', function(done) {
+    tasks(done, {
+        host: "esportivatop.wee.bet",
+        banca: "Esportiva Top",
+        styles: "--header: #002458; --foreground-header: #b1c5e0; --sidebar-right:#011e46; --foreground-sidebar-right: #fff; --sidebar-left: #011e46; --foreground-sidebar-left: #fff; --highlight: yellow; --foreground-highlight: #aec3d8; --odds: #046eef; --foreground-odds: #fff;"
+    });
+});
+
 gulp.task('eurosportbet.wee.bet', function(done) {
     tasks(done, {
         host: "eurosportbet.wee.bet",
