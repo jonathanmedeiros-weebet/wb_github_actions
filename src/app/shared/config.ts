4003d3851ccb602f7ecdce163898543697e3a6a8
Lenildo <lenildo@lti.net.br>
2019-02-16 17:35:13 -0300

    Merge branch 'parametro-local'
    # Conflicts:
    #	src/app/shared/config.ts
    
    config league color

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,8 +1,9 @@
-const _host = 'http://central.demo.wee.bet';
+
+const _host = 'http://superbetsport.com';
 const _center = 'http://api-center2.wee.bet';
 
 export const config: any = {
-    BANCA_NOME: 'DEMO',
+    BANCA_NOME: 'SUPER BET SPORT',
     HOST: _host,
     BASE_URL: `${_host}/api`,
     CENTER_HOST: _center,
