9f4085273cffcb5c2f16d69e4c318d44f39c5597
Thiago Faustino <thiagofaustino51@gmail.com>
2019-02-18 22:15:54 -0300

    erro inesperado

--- src/app/shared/services/utils/error.service.ts
+++ src/app/shared/services/utils/error.service.ts
@@ -12,14 +12,17 @@ export class ErrorService {
 
         if (error.error instanceof ErrorEvent) {
             // A client-side or network error occurred. Handle it accordingly.
-            message = `An error occurred: ${error.error.message}`;
+            message = `Um problema ocorreu: ${error.error.message}`;
             console.error(message);
         } else {
             // The backend returned an unsuccessful response code.
             // The response body may contain clues as to what went wrong,
             // console.error(`Backend returned code ${error.status}, ` + `body was: ${error.message}`);
-
-            message = error.error.errors.message;
+            if (error.error.errors) {
+                message = error.error.errors.message;
+            } else {
+                message = 'Ocorreu um problema inesperado, entre em contato com a banca.';
+            }
         }
         // return an observable with a user-facing error message
         return throwError(message);
