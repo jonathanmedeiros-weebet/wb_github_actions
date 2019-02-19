3323128fc5c4b646bdf0e12988435eef0afd84d5
Lenildo <lenildo@lti.net.br>
2019-02-18 23:03:27 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -190,7 +190,6 @@ gulp.task('mjrsports.com', function (done) {
         host: "mjrsports.com",
         banca: "MJR SPORTS",
         styles: "--header: #3F6826; --foreground-header: #b1c5e0; --sidebar-right: #3B5323; --foreground-sidebar-right: #fff; --sidebar-left: #3B5323; --foreground-sidebar-left: #fff; --highlight: #ff0000; --foreground-highlight: #fff; --odds: #dab600; --foreground-odds: #fff;",
-        old: true
     });
 });
 
@@ -216,7 +215,6 @@ gulp.task('superbets.bet', function (done) {
         host: "superbets.bet",
         banca: "SUPERBETS",
         styles: "--header: #002458; --foreground-header: #b1c5e0; --sidebar-right:#011e46; --foreground-sidebar-right: #fff; --sidebar-left: #011e46; --foreground-sidebar-left: #fff; --highlight: yellow; --foreground-highlight: #aec3d8; --odds: #046eef; --foreground-odds: #fff;",
-        old: true
     });
 });
 
@@ -225,7 +223,6 @@ gulp.task('superbetsport.com', function (done) {
         host: "superbetsport.com",
         banca: "SUPER BET SPORT",
         styles: "--header: #002458; --foreground-header: #b1c5e0; --sidebar-right:#011e46; --foreground-sidebar-right: #fff; --sidebar-left: #011e46; --foreground-sidebar-left: #fff; --highlight: yellow; --foreground-highlight: #aec3d8; --odds: #046eef; --foreground-odds: #fff; --league: yellow;",
-        old: true
     });
 });
 
