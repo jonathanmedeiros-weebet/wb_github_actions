0922d839560418ee6a552a63086f4bccc4e2e576
Lenildo <lenildo@lti.net.br>
2019-03-27 14:32:35 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -185,7 +185,7 @@ gulp.task('gol.vlog.br', function (done) {
 
 gulp.task('maisporte.com', function (done) {
     tasks(done, {
-        host: "maisporte.com",
+        host: "www.maisporte.com",
         banca: "MAISPORTE",
         styles: "--header: #002458; --foreground-header: #b1c5e0; --sidebar-right:#011e46; --foreground-sidebar-right: #fff; --sidebar-left: #011e46; --foreground-sidebar-left: #fff; --highlight: yellow; --foreground-highlight: #aec3d8; --odds: #046eef; --foreground-odds: #fff;",
         old: true
