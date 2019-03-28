38389f03c892eb4042ffd3598d2122f2fc7c5799
Lenildo <lenildo@lti.net.br>
2019-03-27 22:15:49 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -260,6 +260,14 @@ gulp.task('soccerbetting.wee.bet', function (done) {
     });
 });
 
+gulp.task('sportsshow.wee.bet', function (done) {
+    tasks(done, {
+        host: "sportsshow.wee.bet",
+        banca: "Sports Show",
+        styles: "--header: #002458; --foreground-header: #b1c5e0; --sidebar-right:#011e46; --foreground-sidebar-right: #fff; --sidebar-left: #011e46; --foreground-sidebar-left: #fff; --highlight: yellow; --foreground-highlight: #aec3d8; --odds: #046eef; --foreground-odds: #fff;"
+    });
+});
+
 gulp.task('superbets.bet', function (done) {
     tasks(done, {
         host: "superbets.bet",
