d492cebec7c8769791c401532dc4585cf1fde356
Lenildo <lenildo@lti.net.br>
2019-04-05 23:19:33 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -374,3 +374,11 @@ gulp.task('ergol31.wee.bet', function (done) {
         styles: "--header: #002458; --foreground-header: #b1c5e0; --sidebar-right:#011e46; --foreground-sidebar-right: #fff; --sidebar-left: #011e46; --foreground-sidebar-left: #fff; --highlight: yellow; --foreground-highlight: #aec3d8; --odds: #046eef; --foreground-odds: #fff;"
     });
 });
+
+gulp.task('gbaesportes.com', function (done) {
+    tasks(done, {
+        host: "gbaesportes.com",
+        banca: "GBA Esportes",
+        styles: "--header: #002458; --foreground-header: #b1c5e0; --sidebar-right:#011e46; --foreground-sidebar-right: #fff; --sidebar-left: #011e46; --foreground-sidebar-left: #fff; --highlight: yellow; --foreground-highlight: #aec3d8; --odds: #046eef; --foreground-odds: #fff;"
+    });
+});
