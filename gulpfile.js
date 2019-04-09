ed1d82fcc23c0858e3cbbc4db5e8a64ec2a0273d
Lenildo <lenildo@lti.net.br>
2019-04-08 22:04:42 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -395,6 +395,6 @@ gulp.task('betsw2.com', function (done) {
     tasks(done, {
         host: "betsw2.com",
         banca: "Bets W2",
-        styles: "--header: #002458; --foreground-header: #b1c5e0; --sidebar-right:#011e46; --foreground-sidebar-right: #fff; --sidebar-left: #011e46; --foreground-sidebar-left: #fff; --highlight: yellow; --foreground-highlight: #aec3d8; --odds: #046eef; --foreground-odds: #fff;"
+        styles: "--header: #29863b; --foreground-header: #b1c5e0; --sidebar-right:#29863b; --foreground-sidebar-right: #fff; --sidebar-left: #29863b; --foreground-sidebar-left: #fff; --highlight: #9c1915; --foreground-highlight: #aec3d8; --odds: #dbb000; --foreground-odds: #fff;"
     });
 });
