444cebac617182fc3daa7bf33cf8d8a35236977f
Thiago Faustino <thiagofaustino51@gmail.com>
2019-04-10 18:10:25 -0300

    rota do bilhete

---
+++ src/app/bilhete/bilhete.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ApostaEsportivaService } from './../services';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+@Component({
+    selector: 'app-bilhete',
+    templateUrl: 'bilhete.component.html',
+    styleUrls: ['bilhete.component.css']
+})
+export class BilheteComponent implements OnInit, OnDestroy {
+    aposta;
+    unsub$ = new Subject();
+
+    constructor(
+        private route: ActivatedRoute,
+        private apostaEsportivaService: ApostaEsportivaService
+    ) { }
+
+    ngOnInit(
+    ) {
+        this.route.params
+            .subscribe((params: any) => {
+                if (params['id']) {
+                    const id = +params['id'];
+
+                    this.apostaEsportivaService.getAposta(id)
+                        .pipe(takeUntil(this.unsub$))
+                        .subscribe(
+                            apostaEsportiva => {
+                                console.log(apostaEsportiva);
+                                this.aposta = apostaEsportiva;
+                            },
+                            error => console.log(error)
+                        );
+                }
+            });
+
+    }
+
+    ngOnDestroy() {
+        this.unsub$.next();
+        this.unsub$.complete();
+    }
+}
