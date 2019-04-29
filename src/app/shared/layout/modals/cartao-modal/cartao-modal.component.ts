faeba7f02e0e7510be53df5970799eaa8c490ce9
Thiago Faustino <thiagofaustino51@gmail.com>
2019-04-29 18:49:33 -0300

    ajustes

--- src/app/shared/layout/modals/cartao-modal/cartao-modal.component.ts
+++ src/app/shared/layout/modals/cartao-modal/cartao-modal.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { AuthService, PrintService } from './../../../../services';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService, PrintService, ApostaEsportivaService, MessageService } from './../../../../services';
 import { CartaoAposta } from './../../../../models';
+import { ApostaModalComponent } from '../../../layout/modals/aposta-modal/aposta-modal.component';
 
 @Component({
     selector: 'app-cartao-modal',
@@ -12,11 +13,15 @@ import { CartaoAposta } from './../../../../models';
 export class CartaoModalComponent implements OnInit {
     @Input() cartao: CartaoAposta = new CartaoAposta();
     appMobile;
+    modalRef;
 
     constructor(
         public activeModal: NgbActiveModal,
+        public modalService: NgbModal,
         private auth: AuthService,
         private printService: PrintService,
+        private apostaService: ApostaEsportivaService,
+        private messageService: MessageService
     ) { }
 
     ngOnInit() {
@@ -29,5 +34,28 @@ export class CartaoModalComponent implements OnInit {
 
     shared() {
         alert('AINDA NÃO FOI FEITO!');
+    }
+
+    abrirBilhete(id) {
+        this.apostaService.getAposta(id)
+            .subscribe(
+                aposta_localizada => {
+                    this.modalRef = this.modalService.open(ApostaModalComponent, {
+                        ariaLabelledBy: 'modal-basic-title',
+                        centered: true
+                    });
+                    this.modalRef.componentInstance.aposta = aposta_localizada;
+                    this.modalRef.componentInstance.showCancel = true;
+                    this.modalRef.result.then(
+                        (result) => { },
+                        (reason) => { }
+                    );
+                },
+                error => this.handleError(error)
+            );
+    }
+
+    handleError(error) {
+        this.messageService.error(error);
     }
 }
