efc8bd3e8fae40f3501127cb04a3257c20fa5b4a
Lenildo <lenildo@lti.net.br>
2019-05-14 22:30:48 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -561,3 +561,11 @@ gulp.task('onbets.club', function (done) {
         " .inside-event .indentificacao, .inside-event .campeonato-nome, .inside-event .indentificacao-mobile{ background-color: #777; color: white; } .inside-event .tipo-apostas{ background-color: #555!important; color: white!important; } .tipo-aposta { border-top: 1px solid #777!important; } #esportes-footer{ color:white; } "
     });
 });
+
+gulp.task('copaesporte.com', function (done) {
+    tasks(done, {
+        host: "copaesporte.com",
+        banca: "Copa Esporte",
+        styles: "",
+    });
+});
