0004b256172ab2ae64e33ff5aa9685100d5fa171
Thiago Faustino <thiagofaustino51@gmail.com>
2019-05-30 17:05:55 -0300

    ajuste footer

--- src/app/shared/layout/footer/footer.component.ts
+++ src/app/shared/layout/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { config } from '../../config';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: 'footer.component.html'
+    selector: 'app-footer',
+    templateUrl: 'footer.component.html'
 })
 export class FooterComponent implements OnInit {
-  constructor() {}
+    BANCA_NOME = '';
 
-  ngOnInit() {}
+    constructor() { }
+
+    ngOnInit() {
+        this.BANCA_NOME = config.BANCA_NOME;
+    }
 }
