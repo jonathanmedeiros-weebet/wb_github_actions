04b64f497ea53f004d1cd12f33f5841d21b4f940
Lenildo <lenildo@lti.net.br>
2019-07-10 21:33:48 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -507,7 +507,9 @@ gulp.task('redblue.wee.bet', function (done) {
     tasks(done, {
         host: "redblue.wee.bet",
         banca: "Red Blue",
-        styles: "--header: #2538ae; --foreground-header: #fff; --sidebar-right:#2538ae; --foreground-sidebar-right: #fff; --sidebar-left: #2538ae; --foreground-sidebar-left: #fff; --highlight: #ffc107; --foreground-highlight: #fff; --odds: #de1a42; --foreground-odds: #fff; --event-time: #000;",
+        styles: "--header: #333; --foreground-header: #fff; --sidebar-right:#333; --foreground-sidebar-right: #fff; --sidebar-left: #333; --foreground-sidebar-left: #fff; --highlight: #ffdf1b; --foreground-highlight: #ffdf1b; --odds: #2538ae; --foreground-odds: #fff; --event-time: #2538ae; --league: #333; --foreground-league: #fff;",
+        aditional_styles: " #futebol-default-wrapper, #futebol-live-wrapper, #basquete-default-wrapper, #combate-default-wrapper{ background: #555!important } .jogos, .eventos{ background: #555!important; color: #fff!important; } .jogo{ border-top: none!important; } .campeonato-header{ border-bottom-color: #555!important; } .footer{ background-color:#555!important; }" +
+        " .inside-event .indentificacao, .inside-event .campeonato-nome, .inside-event .indentificacao-mobile{ background-color: #2538ae; color: white; } .inside-event .tipo-apostas{ background-color: #555!important; color: white!important; } .tipo-aposta { border-top: 1px solid #777!important; } #esportes-footer{ color:white; } "
     });
 });
 
