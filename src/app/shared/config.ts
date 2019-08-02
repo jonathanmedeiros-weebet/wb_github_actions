35f9f38116f77e44cfeef9e664e5987741ffbd50
Lenildo <lenildo@lti.net.br>
2019-08-02 10:04:33 -0300

    hotfix

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,15 +1,15 @@
 
-const _host = 'http://weebet.local';
-const _center = 'http://localhost:3002';
+const _host = 'http://central.masterbet.wee.bet';
+const _center = 'http://api-center2.wee.bet';
 
 export const config: any = {
-    BANCA_NOME: 'DEMO',
+    BANCA_NOME: 'MasterBet',
     HOST: _host,
     BASE_URL: `${_host}/api`,
     CENTER_HOST: _center,
     CENTER_API: `${_center}/v1`,
     SPORTS_URL: `${_host}/api/esportes`,
     LOTTERIES_URL: `${_host}/api/loteria`,
-    LOGO: 'assets/images/logo_banca.png',
+    LOGO: _host.replace('central.', '')+'/assets/images/logo_banca.png',
     BG: `${_host}/tema/bk.jpg`
 };
