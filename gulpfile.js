6fc171e9f0ebb7539e1f1d044646417287b26d13
Lenildo <lenildo@lti.net.br>
2019-08-06 10:38:33 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -861,7 +861,8 @@ gulp.task('web.betzeta.com', function (done) {
     tasks(done, {
         host: "web.betzeta.com",
         banca: "Bet Zeta",
-        styles: ""
+        styles: "",
+        scripts: "<!--Start of Tawk.to Script--> <script type='text/javascript'> var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); (function(){ var s1=document.createElement('script'),s0=document.getElementsByTagName('script')[0]; s1.async=true; s1.src='https://embed.tawk.to/5d4852bf7d27204601c964d6/default'; s1.charset='UTF-8'; s1.setAttribute('crossorigin','*'); s0.parentNode.insertBefore(s1,s0); })(); </script> <!--End of Tawk.to Script-->"
     });
 });
 
