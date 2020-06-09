df9c1a1d5bdcfddb429b39708c577b59001c1337
Thiago Faustino <thiagofaustino51@gmail.com>
2020-06-09 16:34:31 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -235,9 +235,9 @@ gulp.task('eurosportbet.wee.bet', function (done) {
     });
 });
 
-gulp.task('gol.vlog.br', function (done) {
+gulp.task('gol.wee.bet', function (done) {
     tasks(done, {
-        host: "gol.vlog.br",
+        host: "gol.wee.bet",
         banca: "BRASIL GOL",
         styles: "--header: #4caf50; --foreground-header: #fff; --sidebar-right:#123153; --foreground-sidebar-right: #fff; --sidebar-left: #123153; --foreground-sidebar-left: #fff; --highlight: yellow; --foreground-highlight: #fff; --odds: #4caf50; --foreground-odds: #fff;",
     });
@@ -1004,7 +1004,7 @@ gulp.task('vitoriasports.bet', function (done) {
         host: "vitoriasports.bet",
         banca: "Vitória Sports",
         styles: "--header: #000; --foreground-header: #fff; --sidebar-right:#000; --foreground-sidebar-right: #fff; --sidebar-left: #000; --foreground-sidebar-left: #fff; --highlight: red; --foreground-highlight: yellow; --odds: #292b75; --foreground-odds: #fff; --event-time: red; --league: #333; --foreground-league: #fff;",
-        aditional_styles: " #futebol-default-wrapper, #futebol-live-wrapper, #basquete-default-wrapper, #combate-default-wrapper{ background: #555!important } .jogos, .eventos{ background: #555!important; color: #fff!important; } .jogo{ border-top: none!important; } .campeonato-header{ border-bottom-color: #555!important; } .footer{ background-color:#555!important; }" +
+        aditional_styles: ".sem-evento{color: #fff;} #futebol-default-wrapper, #futebol-live-wrapper, #basquete-default-wrapper, #combate-default-wrapper{ background: #555!important } .jogos, .eventos{ background: #555!important; color: #fff!important; } .jogo{ border-top: none!important; } .campeonato-header{ border-bottom-color: #555!important; } .footer{ background-color:#555!important; }" +
             " .inside-event .indentificacao, .inside-event .campeonato-nome, .inside-event .indentificacao-mobile{ background-color: #cb151c; color: white; } .inside-event .tipo-apostas{ background-color: #555!important; color: white!important; } .tipo-aposta { border-top: 1px solid #777!important; } #esportes-footer{ color:white; } "
     });
 });
