6583ec3689b00116e0be5591f73181198ce4af40
Thiago Faustino <thiagofaustino51@gmail.com>
2020-09-23 11:03:18 -0300

    script

--- gulpfile.js
+++ gulpfile.js
@@ -1845,6 +1845,7 @@ gulp.task('mssportes.wee.bet', function (done) {
         host: "mssportes.wee.bet",
         banca: "MS Sportes",
         styles: "--highlight: #008ef6; --foreground-highlight: #fff; --odds: #ffb701; --foreground-odds: #011e46; --foreground-selected-odds:#fff",
+        scripts: "<!--Start of Tawk.to Script--> <script type='text/javascript'> var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); (function(){ var s1=document.createElement('script'),s0=document.getElementsByTagName('script')[0]; s1.async=true; s1.src='https://embed.tawk.to/5f6b3ee6f0e7167d001303f3/default'; s1.charset='UTF-8'; s1.setAttribute('crossorigin','*'); s0.parentNode.insertBefore(s1,s0); })(); </script> <!--End of Tawk.to Script-->"
     });
 });
 
