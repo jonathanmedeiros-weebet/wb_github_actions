d2b9988319329b7ae80292de1fa5dd510bcdcc57
Thiago Faustino <thiago@lti.net.br>
2020-10-10 14:56:56 -0300

    melhoria

--- src/app/informacoes/informacoes.module.ts
+++ src/app/informacoes/informacoes.module.ts
@@ -1,14 +1,14 @@
-// import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { SharedModule } from './../shared/shared.module';
+import { UtilsModule } from './../shared/utils/utils.module';
 import { InformacoesRoutingModule } from './informacoes-routing.module';
 import { InformacoesComponent } from './informacoes.component';
 import { PaginaService } from './../services';
 
 @NgModule({
     declarations: [InformacoesComponent],
-    imports: [SharedModule, InformacoesRoutingModule],
+    imports: [CommonModule, UtilsModule, InformacoesRoutingModule],
     providers: [PaginaService]
 })
 export class InformacoesModule { }
