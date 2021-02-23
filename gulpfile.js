b8c7edfed5af40fa495482542393052da11ff27a
Thiago Faustino <thiago@lti.net.br>
2021-02-22 23:40:10 -0300

    ajustes cores demo

--- gulpfile.js
+++ gulpfile.js
@@ -220,7 +220,7 @@ gulp.task('demo.wee.bet', function (done) {
     tasks(done, {
         host: "demo.wee.bet",
         banca: "DEMO",
-        styles: ""
+        styles: "--header: #222d32; --foreground-header: #35cc96; --sidebar-right:#1a2226; --foreground-sidebar-right: #fff; --sidebar-left: #1a2226; --foreground-sidebar-left: #fff; --highlight: #35cc96; --foreground-highlight: #d2d6de; --odds: #2c3b41; --foreground-odds: #fff;--foreground-selected-odds: #fff;--selected-event: #1e282c;--event-time: #1e282c;",
     });
 });
 
