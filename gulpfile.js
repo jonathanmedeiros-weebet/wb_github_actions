97856ed8cbc08650020685a8517b697b445c89b5
Thiago Faustino <thiago@lti.net.br>
2021-03-23 09:10:16 -0300

    remove

--- gulpfile.js
+++ gulpfile.js
@@ -2294,14 +2294,6 @@ gulp.task('a-posta.wee.bet', function (done) {
     });
 });
 
-gulp.task('superbetsvip.com', function (done) {
-    tasks(done, {
-        host: "superbetsvip.com",
-        banca: "SUPER BETS VIP",
-        styles: "--header: #222d32; --foreground-header: #c49a3e; --sidebar-right:#1a2226; --foreground-sidebar-right: #fff; --sidebar-left: #1a2226; --foreground-sidebar-left: #fff; --highlight: #c49a3e; --foreground-highlight: #d2d6de; --odds: #2c3b41; --foreground-odds: #fff;--foreground-selected-odds: #fff;--selected-event: #1e282c;--event-time: #1e282c;",
-    });
-});
-
 gulp.task('superbet365.bet', function (done) {
     tasks(done, {
         host: "superbet365.bet",
