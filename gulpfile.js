d75306bd34aa0de78ad9f0b61be6431f2754f44d
Thiago Faustino <thiagofaustino51@gmail.com>
2021-05-12 11:22:05 -0300

    script moralbets

--- gulpfile.js
+++ gulpfile.js
@@ -1161,6 +1161,7 @@ gulp.task('moralbets.com', function (done) {
         host: "moralbets.com",
         banca: "MORAL BETS",
         styles: "--header: #a9a9a9; --foreground-header: #000; --sidebar-right: #a9a9a9; --foreground-sidebar-right: #000; --sidebar-left: #a9a9a9; --foreground-sidebar-left: #000;--highlight:#68eb5a;--foreground-highlight: #000;--odds:#000;",
+        scripts: "<!-- Global site tag (gtag.js) - Google Analytics --><script async src='https://www.googletagmanager.com/gtag/js?id=G-4D549X8JK5'></script><script>  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments);}  gtag('js', new Date());  gtag('config', 'G-4D549X8JK5');</script>"
     });
 });
 
