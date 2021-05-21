0b5e3fd840b4476d56eda84da495f0ac0f1205b2
Thiago Faustino <thiago@lti.net.br>
2021-05-21 17:21:04 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1429,6 +1429,6 @@ gulp.task('top10sportsvip.com', function (done) {
     tasks(done, {
         host: "top10sportsvip.com",
         banca: "TOP 10 SPORTS VIP",
-        styles: "--header: #222d32; --foreground-header: #35cc96; --sidebar-right:#1a2226; --foreground-sidebar-right: #fff; --sidebar-left: #1a2226; --foreground-sidebar-left: #fff; --highlight: #35cc96; --foreground-highlight: #d2d6de; --odds: #2c3b41; --foreground-odds: #fff;--foreground-selected-odds: #fff;--selected-event: #1e282c;--event-time: #1e282c;",
+        styles: "--header: #222d32; --foreground-header: #35cc96; --sidebar-right:#1a2226; --foreground-sidebar-right: #fff; --sidebar-left: #1a2226; --foreground-sidebar-left: #fff; --highlight: #35cc96; --foreground-highlight: #d2d6de; --odds: #2c3b41; --foreground-odds: #fff;--foreground-selected-odds: #fff;--selected-event: #1e282c;--event-time: #1e282c;--league: #35cc96; --foreground-league: #fff;",
     });
 });
