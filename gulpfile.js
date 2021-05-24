dae4e7f11f0bbf2fb420dfdd36a87043eddbea7f
Thiago Faustino <thiago@lti.net.br>
2021-05-24 13:12:09 -0300

    cor

--- gulpfile.js
+++ gulpfile.js
@@ -1437,6 +1437,6 @@ gulp.task('brasilgolbets.com', function (done) {
     tasks(done, {
         host: "brasilgolbets.com",
         banca: "BRASIL GOL BETS",
-        styles: "--header: #222d32; --foreground-header: #05f605; --sidebar-right:#1a2226; --foreground-sidebar-right: #fff; --sidebar-left: #1a2226; --foreground-sidebar-left: #fff; --highlight: #35cc96; --foreground-highlight: #d2d6de; --odds: #2c3b41; --foreground-odds: #fff;--foreground-selected-odds: #fff;--selected-event: #1e282c;--event-time: #1e282c;--league: #05f605; --foreground-league: #fff;",
+        styles: "--header: #222d32; --foreground-header: #009c39; --sidebar-right:#1a2226; --foreground-sidebar-right: #fff; --sidebar-left: #1a2226; --foreground-sidebar-left: #fff; --highlight: #35cc96; --foreground-highlight: #d2d6de; --odds: #2c3b41; --foreground-odds: #fff;--foreground-selected-odds: #fff;--selected-event: #1e282c;--event-time: #1e282c;--league: #009c39; --foreground-league: #fff;",
     });
 });
