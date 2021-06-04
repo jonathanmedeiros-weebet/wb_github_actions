695518c24800d3232ce254f1a8f6db0b6996db2a
Emerson Silva <emersond480@gmail.com>
2021-06-04 14:47:33 -0300

    Configuração de Cores em arquivo externo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,31 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AppComponent implements OnInit {
     @ViewChild('demoModal', { static: true }) demoModal;
+    myVar: string;
 
     constructor(
         private auth: AuthService,
         private parametroService: ParametroService,
         public modalService: NgbModal,
-    ) { }
+    ) {
+        this.myVar = 'black';
+    }
 
     ngOnInit() {
+        document.documentElement.style.setProperty('--header', '#000000');
+        document.documentElement.style.setProperty('--foreground-header', '#35cc96');
+        document.documentElement.style.setProperty('--sidebar-right', '#1a2226');
+        document.documentElement.style.setProperty('--foreground-sidebar-right', '#fff');
+        document.documentElement.style.setProperty('--sidebar-left', '#1a2226');
+        document.documentElement.style.setProperty('--foreground-sidebar-left', '#fff');
+        document.documentElement.style.setProperty('--highlight', '#000000');
+        document.documentElement.style.setProperty('--foreground-highlight', '#d2d6de');
+        document.documentElement.style.setProperty('--odds', '#2c3b41');
+        document.documentElement.style.setProperty('--foreground-odds', '#fff');
+        document.documentElement.style.setProperty('--foreground-selected-odds', '#fff');
+        document.documentElement.style.setProperty('--selected-event', '#1e282c');
+        document.documentElement.style.setProperty('--event-time', '#1e282c');
+
         if (location.search.indexOf('app') >= 0) {
             this.auth.setAppMobile();
         }
