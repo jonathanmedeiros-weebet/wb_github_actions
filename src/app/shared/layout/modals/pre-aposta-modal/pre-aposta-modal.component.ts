8628e9aa11ee86085fd7cb8cf4cd98f8e81e3c86
Thiago Faustino <thiago@lti.net.br>
2021-06-21 16:51:15 -0300

    ajuste

--- src/app/shared/layout/modals/pre-aposta-modal/pre-aposta-modal.component.ts
+++ src/app/shared/layout/modals/pre-aposta-modal/pre-aposta-modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input, ViewChild, ElementRef} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import {AuthService, HelperService, MessageService} from './../../../../services';
