3018788b8ec234a41d7c617df7dc21aeaff091b6
Emerson Silva <emersond480@gmail.com>
2021-06-21 12:15:16 -0300

    Compartilhamento de pré Aposta sem ser Celular.

--- src/app/shared/layout/modals/pre-aposta-modal/pre-aposta-modal.component.ts
+++ src/app/shared/layout/modals/pre-aposta-modal/pre-aposta-modal.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, ViewChild, ElementRef} from '@angular/core';
 
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { AuthService, HelperService } from './../../../../services';
+import {AuthService, HelperService, MessageService} from './../../../../services';
+import {config} from '../../../config';
+let newNavigator: any;
+newNavigator = window.navigator;
 
 @Component({
     selector: 'app-pre-aposta-modal',
@@ -9,13 +12,15 @@ import { AuthService, HelperService } from './../../../../services';
     styleUrls: ['./pre-aposta-modal.component.css']
 })
 export class PreApostaModalComponent implements OnInit {
+    @ViewChild('preAposta', {static: false}) preAposta: ElementRef;
     @Input() codigo;
     appMobile;
 
     constructor(
         public activeModal: NgbActiveModal,
         private auth: AuthService,
-        private helperService: HelperService
+        private helperService: HelperService,
+        private messageService: MessageService
     ) { }
 
     ngOnInit() {
@@ -23,6 +28,17 @@ export class PreApostaModalComponent implements OnInit {
     }
 
     compartilhar() {
-        this.helperService.compartilharPreAposta(this.codigo);
+        if (this.appMobile) {
+            this.helperService.compartilharPreAposta(this.codigo);
+        } else {
+            if (newNavigator.share) {
+                newNavigator.share({
+                    title: config.BANCA_NOME,
+                    text: `${config.BANCA_NOME} PRÉ APOSTA: ${this.codigo.id}`
+                });
+            } else {
+                this.messageService.error('Compartilhamento não suportado pelo seu navegador');
+            }
+        }
     }
 }
