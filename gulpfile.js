f5b27cd74dc409e810260143debc3aa3adad9a1e
Thiago Faustino <thiago@lti.net.br>
2021-09-27 21:01:02 -0300

    remove

--- gulpfile.js
+++ gulpfile.js
@@ -802,14 +802,6 @@ gulp.task('bigbets.website', function (done) {
     });
 });
 
-gulp.task('interior.wee.bet', function (done) {
-    tasks(done, {
-        host: "interior.wee.bet",
-        banca: "INTERIOR BET",
-        styles: "--header: #222d32; --foreground-header: #c49a3e; --sidebar-right:#1a2226; --foreground-sidebar-right: #fff; --sidebar-left: #1a2226; --foreground-sidebar-left: #fff; --highlight: #c49a3e; --foreground-highlight: #d2d6de; --odds: #2c3b41; --foreground-odds: #fff;--foreground-selected-odds: #fff;--selected-event: #1e282c;--event-time: #1e282c;",
-    });
-});
-
 gulp.task('bets085.wee.bet', function (done) {
     tasks(done, {
         host: "bets085.wee.bet",
