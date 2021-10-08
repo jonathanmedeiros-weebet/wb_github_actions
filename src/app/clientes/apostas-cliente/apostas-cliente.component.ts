9004225f1fb387049fbdbb4bcb0c546dbbb12495
Emerson Silva <emerson@lti.net.br>
2021-10-08 13:47:01 -0300

    WIP - Apostas do Cliente

--- src/app/clientes/apostas-cliente/apostas-cliente.component.ts
+++ src/app/clientes/apostas-cliente/apostas-cliente.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BaseFormComponent} from '../../shared/layout/base-form/base-form.component';
+import {FormBuilder, Validators} from '@angular/forms';
+import {MessageService} from '../../shared/services/utils/message.service';
+import * as moment from 'moment';
 
 @Component({
-  selector: 'app-apostas-cliente',
-  templateUrl: './apostas-cliente.component.html',
-  styleUrls: ['./apostas-cliente.component.css']
+    selector: 'app-apostas-cliente',
+    templateUrl: './apostas-cliente.component.html',
+    styleUrls: ['./apostas-cliente.component.css']
 })
-export class ApostasClienteComponent implements OnInit {
+export class ApostasClienteComponent extends BaseFormComponent implements OnInit {
+    queryParams;
+    dataInicial;
+    dataFinal;
+    loteriasHabilitada;
+    acumuladaoHabilitado;
+    desafioHabilitado;
+    activeId = 'esporte';
 
-  constructor() { }
+    constructor(
+        private messageService: MessageService,
+        private fb: FormBuilder
+    ) {
+        super();
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit() {
+        if (moment().day() === 0) {
+            const startWeek = moment().startOf('week');
+            this.dataInicial = startWeek.subtract(6, 'days');
+            this.dataFinal = moment();
+
+        } else {
+            this.dataInicial = moment().startOf('week').add('1', 'day');
+            this.dataFinal = moment();
+        }
+
+
+        this.createForm();
+    }
+
+    createForm() {
+        this.form = this.fb.group({
+            dataInicial: [this.dataInicial.format('YYYY-MM-DD'), Validators.required],
+            dataFinal: [this.dataFinal.format('YYYY-MM-DD'), Validators.required],
+            status: [''],
+        });
+
+        this.submit();
+    }
+
+    handleError(error: string) {
+        this.messageService.error(error);
+    }
+
+    submit() {
+        this.queryParams = this.form.value;
+    }
 
 }
--- src/app/clientes/apostas-cliente/apostas-cliente.module.ts
+++ src/app/clientes/apostas-cliente/apostas-cliente.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { ApostasClienteRoutingModule } from './apostas-cliente-routing.module';
+import {ApostasClienteRoutingModule} from './apostas-cliente-routing.module';
 import {ApostasClienteComponent} from './apostas-cliente.component';
+import {SharedModule} from '../../shared/shared.module';
+import {NgbNavModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
-  declarations: [ApostasClienteComponent],
-  imports: [
-    CommonModule,
-    ApostasClienteRoutingModule
-  ]
+    declarations: [ApostasClienteComponent],
+    imports: [
+        SharedModule,
+        CommonModule,
+        ApostasClienteRoutingModule,
+        NgbNavModule
+    ]
 })
-export class ApostasClienteModule { }
+export class ApostasClienteModule {
+}
