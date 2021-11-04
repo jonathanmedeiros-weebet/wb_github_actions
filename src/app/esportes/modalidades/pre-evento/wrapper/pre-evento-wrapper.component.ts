9ea1ef891559f2155764f9930f711f07ae56e779
Thiago Faustino <thiago@lti.net.br>
2021-11-04 11:10:10 -0300

    ajustes

--- src/app/esportes/modalidades/pre-evento/wrapper/pre-evento-wrapper.component.ts
+++ src/app/esportes/modalidades/pre-evento/wrapper/pre-evento-wrapper.component.ts
@@ -32,88 +32,68 @@ export class PreEventoWrapperComponent implements OnInit, OnDestroy {
     ngOnInit() {
         // this.mobileScreen = window.innerWidth <= 668 ? true : false;
         const dataLimiteTabela = this.paramsService.getOpcoes().data_limite_tabela;
-        this.odds = ['futebol_americano_casa', 'futebol_americano_fora'];
 
         this.route.data
             .pipe(
-                // takeUntil(this.unsub$),
-                // mergeMap((data: any) => {
-                //     console.log(data);
-                //     return this.route.queryParams;
-                // })
                 switchMap((data: any) => {
-                    console.log(data);
+                    this.sportId = data.sportId;
+                    this.setContextoPorEsporte(data.sportId);
+
                     return this.route.queryParams;
-                })
-            )
-            .pipe(
-                tap(() => console.log('xx'))
+                }),
+                takeUntil(this.unsub$)
             )
             .subscribe((params: any) => {
-                console.log(params);
-                // console.log('data');
-                // console.log(data.sportId);
-                // console.log(this.sportId);
-                // if (data.sportId !== this.sportId) {
-                //     this.sportId = data.sportId;
-                //     console.log('xddd');
-                // }
+                this.showLoadingIndicator = true;
+
+                if (params['campeonato']) {
+                    const campeonatoId = params['campeonato'];
+                    const queryParams: any = {
+                        odds: this.odds,
+                        data_final: dataLimiteTabela
+                    };
+
+                    this.campeonatoService.getCampeonato(campeonatoId, queryParams)
+                        .pipe(takeUntil(this.unsub$))
+                        .subscribe(
+                            campeonato => {
+                                this.campeonatos = [campeonato];
+
+                                this.showLoadingIndicator = false;
+                            },
+                            error => this.messageService.error(error)
+                        );
+                } else {
+                    const queryParams: any = {
+                        'sport_id': this.sportId,
+                        'campeonatos_bloqueados': this.paramsService.getCampeonatosBloqueados(),
+                        'odds': this.odds
+                    };
+
+                    if (params['data']) {
+                        const dt = moment(params['data']);
+                        if (dt.isSameOrBefore(dataLimiteTabela, 'day')) {
+                            queryParams.data = dt.format('YYYY-MM-DD');
+                        } else {
+                            queryParams.data = dataLimiteTabela;
+                        }
+                    } else {
+                        queryParams.data = moment().format('YYYY-MM-DD');
+                    }
+
+                    this.campeonatoService.getCampeonatos(queryParams)
+                        .pipe(takeUntil(this.unsub$))
+                        .subscribe(
+                            campeonatos => {
+                                console.log('campeonatos');
+                                console.log(campeonatos);
+                                this.campeonatos = campeonatos;
+                                this.showLoadingIndicator = false;
+                            },
+                            error => this.messageService.error(error)
+                        );
+                }
             });
-
-        // this.route.queryParams
-        //     .pipe(takeUntil(this.unsub$))
-        //     .subscribe((params: any) => {
-        //         console.log('params', params);
-        //         this.showLoadingIndicator = true;
-
-        //         if (params['campeonato']) {
-        //             const campeonatoId = params['campeonato'];
-        //             const queryParams: any = {
-        //                 odds: this.odds,
-        //                 data_final: dataLimiteTabela
-        //             };
-
-        //             this.campeonatoService.getCampeonato(campeonatoId, queryParams)
-        //                 .pipe(takeUntil(this.unsub$))
-        //                 .subscribe(
-        //                     campeonato => {
-        //                         this.campeonatos = [campeonato];
-
-        //                         this.showLoadingIndicator = false;
-        //                     },
-        //                     error => this.messageService.error(error)
-        //                 );
-        //         } else {
-        //             const queryParams: any = {
-        //                 'sport_id': 12,
-        //                 'campeonatos_bloqueados': this.paramsService.getCampeonatosBloqueados(),
-        //                 'odds': this.odds
-        //             };
-
-        //             if (params['data']) {
-        //                 const dt = moment(params['data']);
-        //                 if (dt.isSameOrBefore(dataLimiteTabela, 'day')) {
-        //                     queryParams.data = dt.format('YYYY-MM-DD');
-        //                 } else {
-        //                     queryParams.data = dataLimiteTabela;
-        //                 }
-        //             } else {
-        //                 queryParams.data = moment().format('YYYY-MM-DD');
-        //             }
-
-        //             this.campeonatoService.getCampeonatos(queryParams)
-        //                 .pipe(takeUntil(this.unsub$))
-        //                 .subscribe(
-        //                     campeonatos => {
-        //                         console.log('campeonatos');
-        //                         console.log(campeonatos);
-        //                         this.campeonatos = campeonatos;
-        //                         this.showLoadingIndicator = false;
-        //                     },
-        //                     error => this.messageService.error(error)
-        //                 );
-        //         }
-        //     });
     }
 
     ngOnDestroy() {
@@ -125,7 +105,7 @@ export class PreEventoWrapperComponent implements OnInit, OnDestroy {
         const campeonatosBloqueados = this.paramsService.getCampeonatosBloqueados();
         const opcoes = this.paramsService.getOpcoes();
         const params = {
-            'sport_id': 18,
+            'sport_id': this.sportId,
             'campeonatos_bloqueados': campeonatosBloqueados,
             'data_final': opcoes.data_limite_tabela,
         };
@@ -133,8 +113,30 @@ export class PreEventoWrapperComponent implements OnInit, OnDestroy {
         this.campeonatoService.getCampeonatos(params)
             .pipe(takeUntil(this.unsub$))
             .subscribe(
-                campeonatos => this.sidebarService.changeItens(campeonatos, 'basquete'),
+                campeonatos => this.sidebarService.changeItens(this.campeonatos, this.contexto),
                 error => this.messageService.error(error)
             );
+    }
+
+    setContextoPorEsporte(sportId) {
+        switch (sportId) {
+            case '1':
+                this.contexto = 'futebol';
+                this.odds = ['casa_90', 'empate_90', 'fora_90'];;
+                if (this.paramsService.getOddsPrincipais()) {
+                    this.odds = this.paramsService.getOddsPrincipais();
+                }
+                break;
+            case '9':
+                this.contexto = 'combate';
+                this.odds = ['cmbt_casa', 'cmbt_fora'];
+                break;
+            case '12':
+                this.contexto = 'futebol-americano';
+                this.odds = ['futebol_americano_casa', 'futebol_americano_fora'];
+                break;
+            default:
+                break;
+        }
     }
 }
