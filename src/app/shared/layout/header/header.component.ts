2cf0d28e40cd5f283ba14fc953d52ef3b659bd2e
Emerson Silva <emerson@lti.net.br>
2021-12-03 15:19:17 -0300

    WIP

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
 import {animate, state, style, transition, trigger} from '@angular/animations';
 
@@ -55,6 +55,10 @@ export class HeaderComponent extends BaseFormComponent implements OnInit, OnDest
     isCliente;
     modoClienteAtivo;
     contatoSolicitacaoSaque;
+    menuWidth;
+    @ViewChild('scrollMenu') scrollMenu: ElementRef;
+    rightDisabled: boolean = false;
+    leftDisabled: boolean = true;
 
     constructor(
         private fb: FormBuilder,
@@ -104,11 +108,36 @@ export class HeaderComponent extends BaseFormComponent implements OnInit, OnDest
 
         this.getUsuario();
         this.createForm();
+
+        this.menuWidth = window.innerWidth - 500;
     }
 
     ngOnDestroy() {
         this.unsub$.next();
         this.unsub$.complete();
+    }
+
+    scrollLeft() {
+        this.scrollMenu.nativeElement.scrollLeft -= 150;
+        this.checkScroll();
+    }
+
+    scrollRight() {
+        this.scrollMenu.nativeElement.scrollLeft += 150;
+        this.checkScroll();
+    }
+
+    onScroll(e) {
+        this.checkScroll();
+    }
+
+    checkScroll() {
+        this.scrollMenu.nativeElement.scrollLeft == 0 ? this.leftDisabled = true : this.leftDisabled = false;
+
+        let newScrollLeft = this.scrollMenu.nativeElement.scrollLeft;
+        let width = this.scrollMenu.nativeElement.clientWidth;
+        let scrollWidth = this.scrollMenu.nativeElement.scrollWidth;
+        scrollWidth - (newScrollLeft + width) == 0 ? this.rightDisabled = true : this.rightDisabled = false;
     }
 
     createForm() {
