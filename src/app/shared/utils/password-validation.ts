09fc4e37271bf04e649fe045a7a91f1be0301ddc
Emerson Silva <emerson@lti.net.br>
2021-12-15 14:56:59 -0300

    Correção de Recuperação de Senha

--- src/app/shared/utils/password-validation.ts
+++ src/app/shared/utils/password-validation.ts
@@ -4,7 +4,7 @@ export class PasswordValidation {
 
     static MatchPassword(AC: AbstractControl) {
         let valid = false;
-        const passwordControl = AC.get('password') || AC.get('new_password') || AC.get('senha') || AC.get('senha_nova');
+        const passwordControl = AC.get('password') || AC.get('new_password') || AC.get('senha') || AC.get('senha_nova') || AC.get('nova_senha');
         const confirmControl = AC.get('confirm') || AC.get('confirmation_password') || AC.get('senha_confirmacao');
 
         if ((passwordControl.valid && passwordControl.dirty) && (confirmControl.valid && confirmControl.dirty)) {
