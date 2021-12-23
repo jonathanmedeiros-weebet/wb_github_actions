922c88dcd5173a12dfd750098064df5e260adb52
Thiago Faustino <thiago@lti.net.br>
2021-12-23 18:39:07 -0300

    ajustes

--- src/app/esportes/destaques/destaques.component.ts
+++ src/app/esportes/destaques/destaques.component.ts
@@ -1,6 +1,5 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {Router} from '@angular/router';
-import {UtilsService} from "../../shared/services/utils/utils.service";
+import { Component, EventEmitter, OnInit, Output, ChangeDetectorRef } from '@angular/core';
+import { UtilsService } from "../../shared/services/utils/utils.service";
 
 @Component({
     selector: 'app-destaques',
@@ -12,22 +11,26 @@ export class DestaquesComponent implements OnInit {
     regioesDestaque = null;
     exibindoRegiao = false;
     menuWidth;
+    mobileScreen;
 
     constructor(
-        private router: Router,
         private utilsService: UtilsService,
-    ) {
-    }
+        private cd: ChangeDetectorRef
+    ) { }
 
     ngOnInit() {
+        this.mobileScreen = window.innerWidth <= 1024 ? true : false;
+
         this.utilsService.getRegioesDestaque()
             .subscribe(
                 res => {
-                    if (res.length > 0){
+                    if (res.length > 0) {
                         this.regioesDestaque = res;
+                        this.cd.detectChanges();
                     }
                 }
             );
+
         this.menuWidth = window.innerWidth - 10;
     }
 
@@ -39,5 +42,15 @@ export class DestaquesComponent implements OnInit {
             this.regiaoSelecionada.emit(siglaRegiao);
             this.exibindoRegiao = true;
         }
+    }
+
+    exibirDestaques() {
+        let result = false
+
+        if (this.mobileScreen && this.regioesDestaque && !this.exibindoRegiao) {
+            result = true;
+        }
+
+        return result;
     }
 }
