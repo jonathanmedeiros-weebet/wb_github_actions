7bff9f3823e69c8800e6ee5203016b61a6b7dafa
Emerson Silva <emersond480@gmail.com>
2022-01-21 10:34:53 -0300

    Limpeza de componentes

--- src/app/clientes/deposito/deposito-routing.module.ts
+++ src/app/clientes/deposito/deposito-routing.module.ts
@@ -19,14 +19,6 @@ const routes: Routes = [
             {
                 path: 'whatsapp',
                 loadChildren: () => import('./deposito-whatsapp/deposito-whatsapp.module').then(m => m.DepositoWhatsappModule)
-            },
-            {
-                path: 'boleto',
-                loadChildren: () => import('./deposito-boleto/deposito-boleto.module').then(m => m.DepositoBoletoModule)
-            },
-            {
-                path: 'cartao',
-                loadChildren: () => import('./deposito-cartao/deposito-cartao.module').then(m => m.DepositoCartaoModule)
             }
         ]
     },
