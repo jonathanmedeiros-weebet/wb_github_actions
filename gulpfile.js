5d543561ba7a3ab1d94b79c5037e8cc3ff08bc5e
Thiago Faustino <thiago@lti.net.br>
2022-02-21 11:15:23 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1916,3 +1916,12 @@ gulp.task('esportenetreipele.com', function (done) {
         styles: "",
     });
 });
+
+gulp.task('jogadamagica.wee.bet', function (done) {
+    tasks(done, {
+        server: "front2.wee.bet",
+        host: "jogadamagica.wee.bet",
+        banca: "JOGADA MÁGICA",
+        styles: "",
+    });
+});
