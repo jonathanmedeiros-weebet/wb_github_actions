a073a7eb3e340ac6f13fa75cfd80a31ab4b043ae
Thiago Faustino <thiago@lti.net.br>
2022-03-21 15:17:05 -0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1883,7 +1883,8 @@ gulp.task('betfacil123.app', function (done) {
 gulp.task('newbet.wee.bet', function (done) {
     tasks(done, {
         server: "front2.wee.bet",
-        host: "newb10.net",
+        host: "newbet.wee.bet",
+        shared_url: "newb10.net",
         banca: "NEW BET",
         styles: "",
     });
