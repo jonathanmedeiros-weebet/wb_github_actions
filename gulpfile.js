3cf963d841de41cab07bda82733bf3b8106c6ea3
Thiago Faustino <thiago@lti.net.br>
2022-03-23 19:33:14 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -845,7 +845,10 @@ gulp.task('moralbets.com', function (done) {
         host: "moralbets.com",
         banca: "MORAL BETS",
         styles: "--header: #a9a9a9; --foreground-header: #000; --sidebar-right: #a9a9a9; --foreground-sidebar-right: #000; --sidebar-left: #a9a9a9; --foreground-sidebar-left: #000;--highlight:#68eb5a;--foreground-highlight: #000;--odds:#000;",
-        scripts: "<!-- Global site tag (gtag.js) - Google Analytics --><script async src='https://www.googletagmanager.com/gtag/js?id=G-4D549X8JK5'></script><script>  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments);}  gtag('js', new Date());  gtag('config', 'G-4D549X8JK5');</script>"
+        scripts: "\
+        <!-- Global site tag (gtag.js) - Google Analytics --><script async src='https://www.googletagmanager.com/gtag/js?id=G-4D549X8JK5'></script><script>  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments);}  gtag('js', new Date());  gtag('config', 'G-4D549X8JK5');</script>\
+        <!-- Meta Pixel Code --><script>  !function(f,b,e,v,n,t,s)  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?  n.callMethod.apply(n,arguments):n.queue.push(arguments)};  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';  n.queue=[];t=b.createElement(e);t.async=!0;  t.src=v;s=b.getElementsByTagName(e)[0];  s.parentNode.insertBefore(t,s)}(window, document,'script',  'https://connect.facebook.net/en_US/fbevents.js');  fbq('init', '964415314440990');  fbq('track', 'PageView');</script><noscript><img height='1' width='1' style='display:none'  src='https://www.facebook.com/tr?id=964415314440990&ev=PageView&noscript=1'/></noscript><!-- End Meta Pixel Code -->\
+        "
     });
 });
 
@@ -2083,6 +2086,24 @@ gulp.task('bets084.com', function (done) {
         server: "front2.wee.bet",
         host: "bets084.com",
         banca: "BETS 084",
+        styles: "",
+    });
+});
+
+gulp.task('betmatch.wee.bet', function (done) {
+    tasks(done, {
+        server: "front1.wee.bet",
+        host: "betmatch.wee.bet",
+        banca: "BETMATCH",
+        styles: "",
+    });
+});
+
+gulp.task('betson.wee.bet', function (done) {
+    tasks(done, {
+        server: "front1.wee.bet",
+        host: "betson.wee.bet",
+        banca: "BETSON",
         styles: "",
     });
 });
