17dc7bd1dda4b70b58d79ad331e89b17e3d602f2
Emerson Silva <emerson@lti.net.br>
2022-04-12 09:00:17 -0300

    Remoção de Script de Chat BetPixBrasil.com

--- gulpfile.js
+++ gulpfile.js
@@ -1728,7 +1728,7 @@ gulp.task('betpixbrasil.com', function (done) {
         host: "betpixbrasil.com",
         banca: "BET PIX BRASIL",
         styles: "",
-        scripts: "<!--Start of Tawk.to Script--> <script type='text/javascript'> var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); (function(){ var s1=document.createElement('script'),s0=document.getElementsByTagName('script')[0]; s1.async=true; s1.src='https://embed.tawk.to/621c37121ffac05b1d7c2742/1fsv3e8ta'; s1.charset='UTF-8'; s1.setAttribute('crossorigin','*'); s0.parentNode.insertBefore(s1,s0); })(); </script> <!--End of Tawk.to Script-->"
+        scripts: ""
     });
 });
 
