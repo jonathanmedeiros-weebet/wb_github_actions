b067a44ca4ef6f6ad24d20a58d65c0af9481613d
Emerson Silva <emerson@lti.net.br>
2022-04-25 11:33:16 -0300

    Correção de erro

--- gulpfile.js
+++ gulpfile.js
@@ -1721,7 +1721,7 @@ gulp.task('betshelby.net', function (done) {
 gulp.task('esportenetreipele.com', function (done) {
     tasks(done, {
         server: "front3.wee.bet",
-        host: "esportenetreipele.com",npmr
+        host: "esportenetreipele.com",
         banca: "ESPORTE NET",
         styles: "",
         scripts: "<!--Start of Tawk.to Script--> <script type='text/javascript'> var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); (function(){ var s1=document.createElement('script'),s0=document.getElementsByTagName('script')[0]; s1.async=true; s1.src='https://embed.tawk.to/623500f51ffac05b1d7f4f2c/1fufgnh2n'; s1.charset='UTF-8'; s1.setAttribute('crossorigin','*'); s0.parentNode.insertBefore(s1,s0); })(); </script> <!--End of Tawk.to Script-->"
