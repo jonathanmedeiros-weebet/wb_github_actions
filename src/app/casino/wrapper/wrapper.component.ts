53cbc19c04d27791928ecc6960513268c1901f78
Tawan Carvalho <tawan.carvalho@lti.net.br>
2022-05-30 13:54:05 -0300

    Ocultar footerMenu no casino

--- src/app/casino/wrapper/wrapper.component.ts
+++ src/app/casino/wrapper/wrapper.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { CasinoApiService } from 'src/app/shared/services/casino/casino-api.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {CasinoApiService} from 'src/app/shared/services/casino/casino-api.service';
 import {SidebarService} from '../../shared/services/utils/sidebar.service';
+import {MenuFooterService} from '../../services';
 
 @Component({
-  selector: 'app-wrapper',
-  templateUrl: './wrapper.component.html',
-  styleUrls: ['./wrapper.component.css']
+    selector: 'app-wrapper',
+    templateUrl: './wrapper.component.html',
+    styleUrls: ['./wrapper.component.css']
 })
-export class CasinoWrapperComponent implements OnInit {
+export class CasinoWrapperComponent implements OnInit, OnDestroy {
 
-  constructor( private sideBarService: SidebarService ) { }
-
-  ngOnInit(): void {
-      this.sideBarService.changeItens({
-          contexto:'casino',
-          dados:{}
-
-      })
-
-  }
+    constructor(
+        private sideBarService: SidebarService,
+        private menuFooterService: MenuFooterService,
+    ) {}
+    ngOnInit(): void {
+        this.sideBarService.changeItens({
+            contexto: 'casino',
+            dados: {}
+        });
+        this.menuFooterService.setIsPagina(true);
+    }
+    ngOnDestroy() {
+        this.menuFooterService.setIsPagina(false);
+    }
 
 }
