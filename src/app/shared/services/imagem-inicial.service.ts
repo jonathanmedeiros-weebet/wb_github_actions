7d96666d427726211c3c4540c7d2021352eb3975
Tawan Carvalho <tawan.carvalho@lti.net.br>
2022-07-22 17:15:33 -0300

    commit

---
+++ src/app/shared/services/imagem-inicial.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { catchError, take, map } from 'rxjs/operators';
+
+import { HeadersService } from './utils/headers.service';
+import { ErrorService } from './utils/error.service';
+import { Pagina } from '../../models';
+import { config } from '../config';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ImagemInicialService {
+    private ImagemInicialUrl = `${config.BASE_URL}/imageminicial`;
+
+    constructor(
+        private http: HttpClient,
+        private header: HeadersService,
+        private errorService: ErrorService
+    ) { }
+
+    getImagens(): Observable<any> {
+        return this.http.get(this.ImagemInicialUrl, this.header.getRequestOptions(true))
+            .pipe(
+                take(1),
+                map((res: any) => res.results),
+                catchError(this.errorService.handleError)
+            );
+    }
+}
