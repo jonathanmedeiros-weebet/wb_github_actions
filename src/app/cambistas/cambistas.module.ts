c3a10f4718906b91c97e88918c0ab947a9d38c52
Tawan Carvalho <tawan.carvalho@lti.net.br>
2022-08-16 11:18:32 -0300

    ajuste

--- src/app/cambistas/cambistas.module.ts
+++ src/app/cambistas/cambistas.module.ts
@@ -1,5 +1,4 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {SharedModule} from '../shared/shared.module';
 import {CambistasRoutingModule} from './cambistas-routing.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
@@ -7,7 +6,6 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
     declarations: [],
     imports: [
-        CommonModule,
         SharedModule,
         CambistasRoutingModule,
         NgbModule
