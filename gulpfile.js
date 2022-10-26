467f1fc3a0a41474a658c8f15a47f74ecf45a110
Emerson Silva <emerson.silva@lti.net.br>
2022-10-26 11:13:47 -0300

    Ajuste Jivosite Betbetix.com

--- gulpfile.js
+++ gulpfile.js
@@ -2378,6 +2378,7 @@ gulp.task('betbetix.com', function (done) {
         host: "betbetix.com",
         banca: "BETBETIX",
         styles: "",
+        pixel: "<link rel='stylesheet' href='https://cdn.wee.bet/jivosite/jivosite.css'>",
         scripts: "<script src='https://cdn.wee.bet/jivosite/jivosite.js' type='text/javascript'><script src='//code.jivosite.com/widget/5WSOiOS6b1' async></script>"
     });
 });
