962b52fca2f684f6098b40740e67a942c4678e1f
Emerson Silva <emerson.silva@lti.net.br>
2022-11-17 12:09:24 -0300

    remoção wrapper desnecessário / ajuste form acumuladão

--- src/app/acumuladao/acumuladao.module.ts
+++ src/app/acumuladao/acumuladao.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 
 import { AcumuladaoRoutingModule } from './acumuladao-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { AcumuladaoWrapperComponent } from './acumuladao-wrapper/acumuladao-wrapper.component';
 import { AcumuladaoListagemComponent } from './acumuladao-listagem/acumuladao-listagem.component';
 import { AcumuladaoFormComponent } from './acumuladao-form/acumuladao-form.component';
 import { AcumuladaoService } from '../services';
@@ -15,7 +14,6 @@ import { NgxMaskModule } from 'ngx-mask';
         NgxMaskModule.forRoot()
     ],
     declarations: [
-        AcumuladaoWrapperComponent,
         AcumuladaoListagemComponent,
         AcumuladaoFormComponent
     ],
