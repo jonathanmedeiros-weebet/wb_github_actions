5bf6a5228a88cd4a20b74ea4635a3fe239046a27
Iuri Brito <iuri.brito@lti.net.br>
2022-12-01 11:03:48 -0300

    removendo comentarios

--- src/app/shared/services/campinho.service.ts
+++ src/app/shared/services/campinho.service.ts
@@ -31,35 +31,5 @@ export class CampinhoService {
                 map((res: any) => res.results),
                 catchError(this.errorService.handleError)
             );
-
-        // const headers = {
-        //     'Content-Type': 'application/json'
-        // }
-
-        // const user = 'betwee'
-        // const secret = "343171fb6c93003203de52a99043f16e"
-        // const params = { user, secret }
-
-
-        // return this.http.get('https://api.thesports.com/v1/football/bet365/match/list', { headers, params })
-        //     .pipe(
-        //         take(1),
-        //         map((res: any) => res.results),
-        //         catchError(this.errorService.handleError)
-        //     );
-
-
-        // return from(
-        //     fetch(
-        //         'https://api.thesports.com/v1/football/bet365/match/list?user=betwee&secret=343171fb6c93003203de52a99043f16e', // the url you are trying to access
-        //         {
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //         },
-        //         method: 'GET', // GET, POST, PUT, DELETE
-        //         mode: 'no-cors' // the most important option
-        //         }
-        //     )
-        // );
     }
 }
