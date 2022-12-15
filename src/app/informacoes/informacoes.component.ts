0ea871d846c511dae403de7d88a4dd6ef39d5403
Emerson Silva <emerson.silva@lti.net.br>
2022-12-15 11:41:59 -0300

    ajuste

--- src/app/informacoes/informacoes.component.ts
+++ src/app/informacoes/informacoes.component.ts
@@ -1,32 +1,24 @@
-import {
-    Component,
-    OnInit,
-    OnDestroy,
-    Renderer2,
-    ElementRef,
-    ChangeDetectorRef,
-    AfterViewInit,
-    ViewChild,
-} from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import {Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild,} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
-import { Subject } from "rxjs";
-import { takeUntil } from "rxjs/operators";
-import { MenuFooterService, PaginaService } from "./../services";
-import { Pagina } from "./../models";
-import { TranslateService } from "@ngx-translate/core";
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+import {MenuFooterService, PaginaService} from './../services';
+import {Pagina} from './../models';
+import {TranslateService} from '@ngx-translate/core';
 
-import pdfMake from 'pdfmake/build/pdfmake';
+import {pdfMake} from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
+import {htmlToPdfmake} from 'html-to-pdfmake';
+
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
-import htmlToPdfmake from 'html-to-pdfmake';
 
 @Component({
-    templateUrl: "./informacoes.component.html",
-    styleUrls: ["informacoes.component.css"],
+    templateUrl: './informacoes.component.html',
+    styleUrls: ['informacoes.component.css'],
 })
 export class InformacoesComponent implements OnInit, OnDestroy {
-    @ViewChild("content") content: ElementRef;
+    @ViewChild('content') content: ElementRef;
     showLoadingIndicator = true;
     pagina = new Pagina();
     conteudo;
@@ -41,19 +33,20 @@ export class InformacoesComponent implements OnInit, OnDestroy {
         private renderer: Renderer2,
         private el: ElementRef,
         private translate: TranslateService
-    ) {}
+    ) {
+    }
 
     ngOnInit() {
         this.mobileScreen = window.innerWidth <= 1024;
 
         this.route.data.pipe(takeUntil(this.unsub$)).subscribe((data) => {
             this.tituloPagina = this.translate.instant(
-                "paginas." + data.pagina
+                'paginas.' + data.pagina
             );
             this.translate.onLangChange.subscribe(
                 () =>
                     (this.tituloPagina = this.translate.instant(
-                        "paginas." + data.pagina
+                        'paginas.' + data.pagina
                     ))
             );
             this.paginaService
@@ -69,15 +62,15 @@ export class InformacoesComponent implements OnInit, OnDestroy {
 
     definirAltura() {
         const altura = window.innerHeight - 105;
-        const wrapStickyEl = this.el.nativeElement.querySelector(".wrap-stick");
-        this.renderer.setStyle(wrapStickyEl, "min-height", `${altura}px`);
+        const wrapStickyEl = this.el.nativeElement.querySelector('.wrap-stick');
+        this.renderer.setStyle(wrapStickyEl, 'min-height', `${altura}px`);
     }
 
     savePDF() {
         const content = this.content.nativeElement.innerHTML.replace(/(style=".+?")/gm, '');
         const html = htmlToPdfmake(`<h2>${this.tituloPagina}</h2>` + content);
 
-        const documentDefinition = { content: html };
+        const documentDefinition = {content: html};
         pdfMake.createPdf(documentDefinition).download(this.pagina.chave + '.pdf');
     }
 
