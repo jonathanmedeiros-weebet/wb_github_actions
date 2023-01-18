0c09a034a2c3c1261b295969a669a4a5c510e448
Emerson Silva <emerson.silva@lti.net.br>
2023-01-18 09:54:20 -0300

    adicão script primoos.bet

--- gulpfile.js
+++ gulpfile.js
@@ -2452,6 +2452,7 @@ gulp.task('primoos.bet', function (done) {
         server: "front2.wee.bet",
         host: "primoos.bet",
         banca: "PRIMOOS.BET",
+        pixel: "<!-- Meta Pixel Code --><script>    !function(f,b,e,v,n,t,s)    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?    n.callMethod.apply(n,arguments):n.queue.push(arguments)};    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';    n.queue=[];t=b.createElement(e);t.async=!0;    t.src=v;s=b.getElementsByTagName(e)[0];    s.parentNode.insertBefore(t,s)}(window, document,'script',    'https://connect.facebook.net/en_US/fbevents.js');    fbq('init', '2468428439977041');    fbq('track', 'PageView');    </script>    <noscript><img height='1' width='1' style='display:none'    src='https://www.facebook.com/tr?id=2468428439977041&ev=PageView&noscript=1'    /></noscript>    <!-- End Meta Pixel Code -->",
         styles: "",
     });
 });
