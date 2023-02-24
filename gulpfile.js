490b507650073976d3dd798e0d075758d1334051
Thiago Faustino <thiago@lti.net.br>
2023-02-24 19:51:09 -0300

    remove

--- gulpfile.js
+++ gulpfile.js
@@ -1088,15 +1088,6 @@ gulp.task('betcompany.wee.bet', function (done) {
     });
 });
 
-gulp.task('trevoone.bet', function (done) {
-    tasks(done, {
-        server: "front3.wee.bet",
-        host: "trevoone.bet",
-        banca: "TREVO ONE",
-        styles: "",
-    });
-});
-
 gulp.task('dfsportsbet.wee.bet', function (done) {
     tasks(done, {
         server: "front2.wee.bet",
@@ -2058,17 +2049,6 @@ gulp.task('betfut365.bet', function (done) {
     });
 });
 
-gulp.task('365club.app', function (done) {
-    tasks(done, {
-        server: "front3.wee.bet",
-        host: "365club.app",
-        banca: "365 CLUB",
-        styles: "",
-        google_tag_part_1: "<!-- Google Tag Manager --> <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-KRF4XSR');</script> <!-- End Google Tag Manager -->",
-        google_tag_part_2: "<!-- Google Tag Manager (noscript) --> <noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-KRF4XSR' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript> <!-- End Google Tag Manager (noscript) -->"
-    });
-});
-
 gulp.task('betbrasil.one', function (done) {
     tasks(done, {
         server: "front1.wee.bet",
@@ -2265,16 +2245,6 @@ gulp.task('3tbet.com.br', function (done) {
     });
 });
 
-gulp.task('startbet.com.br', function (done) {
-    tasks(done, {
-        server: "front2.wee.bet",
-        host: "startbet.com.br",
-        banca: "START BET",
-        styles: "",
-        google_tag_part_1: "<!-- Google tag (gtag.js) --><script async src=''></script><script>  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments);}  gtag('js', new Date());  gtag('config', 'G-CL4H2K1W87');</script>",
-    });
-});
-
 gulp.task('nacionalbets.com', function (done) {
     tasks(done, {
         server: "front2.wee.bet",
@@ -2289,15 +2259,6 @@ gulp.task('betnazebra.com.br', function (done) {
         server: "front1.wee.bet",
         host: "betnazebra.com.br",
         banca: "BETNAZEBRA",
-        styles: "",
-    });
-});
-
-gulp.task('cashsportes.bet', function (done) {
-    tasks(done, {
-        server: "front3.wee.bet",
-        host: "cashsportes.bet",
-        banca: "CASH SPORTES",
         styles: "",
     });
 });
@@ -2445,15 +2406,6 @@ gulp.task('wizz.bet', function (done) {
         banca: "WIZZ.BET",
         styles: "",
         google_tag_part_1: "<!-- Google tag (gtag.js) --> <script async src='https://www.googletagmanager.com/gtag/js?id=G-9P3GHF5XD9'></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-9P3GHF5XD9'); </script>"
-    });
-});
-
-gulp.task('jogomilionario.com', function (done) {
-    tasks(done, {
-        server: "front3.wee.bet",
-        host: "jogomilionario.com",
-        banca: "JOGO MILIONÁRIO",
-        styles: "",
     });
 });
 
