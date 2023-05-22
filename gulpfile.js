dd7d2a408591a0edda4676a7e741e398bcaab731
Gustavo Andrade <gustavoandradedeveloper@gmail.com>
2023-05-22 08:32:58 -0300

    Removi o jivosite do cliente mzsports.pro

--- gulpfile.js
+++ gulpfile.js
@@ -2028,8 +2028,8 @@ gulp.task('mzsports.pro', function (done) {
         host: "mzsports.pro",
         banca: "MZ SPORTS",
         styles: "",
-        pixel: "<link rel='stylesheet' href='https://cdn.wee.bet/jivosite/jivosite.css'>",
-        scripts: "<script src='https://cdn.wee.bet/jivosite/jivosite.js' type='text/javascript'></script><script src='//code.jivosite.com/widget/oTpmAU83Z4' async></script>"
+        pixel: "",
+        scripts: ""
     });
 });
 
