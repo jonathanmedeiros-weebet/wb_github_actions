53e1d0651a0839f25fe0152523a6b7bc0ae54099
Emerson Silva <emerson.silva@lti.net.br>
2023-06-28 08:38:54 -0300

    tags e ajustes deploy

--- gulpfile.js
+++ gulpfile.js
@@ -2751,7 +2751,7 @@ gulp.task('favoritao.com', function (done) {
     tasks(done, {
         server: "front4.wee.bet",
         host: "favoritao.com",
-        banca: "FAVORITÃO",
+        banca: "Favoritão.bet - Onde os favoritos se encontram!",
         styles: "",
         pixel: "<!-- Meta Pixel Code --><script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '2339542659563497');fbq('track', 'PageView');</script><noscript><img height='1' width='1' style='display:none'src='https://www.facebook.com/tr?id=2339542659563497&ev=PageView&noscript=1'/></noscript><!-- End Meta Pixel Code -->",
     });
@@ -2880,6 +2880,7 @@ gulp.task('24horas.bet', function (done) {
         host: "24horas.bet",
         banca: "24HORAS.BET",
         styles: "",
+        google_tag_part_2: "<!-- Google tag (gtag.js) --><script async src='https://www.googletagmanager.com/gtag/js?id=G-ZFDDC0CH6S'></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-ZFDDC0CH6S');</script>"
     });
 });
 
