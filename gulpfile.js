15da3ebfdc6cf8f0cc9d2fedd679cb7ec8447fa1
Thiago Faustino <thiago.faustino@lti.net.br>
2023-08-10 11:20:41 -0300

    remove

--- gulpfile.js
+++ gulpfile.js
@@ -565,19 +565,6 @@ gulp.task('dolarbets.wee.bet', function (done) {
     });
 });
 
-gulp.task('mssportes.wee.bet', function (done) {
-    tasks(done, {
-        server: "front2.wee.bet",
-        host: "mssportes.wee.bet",
-        banca: "MS Sportes",
-        styles: "--highlight: #008ef6; --foreground-highlight: #fff; --odds: #ffb701; --foreground-odds: #011e46; --foreground-selected-odds:#fff",
-        aditional_styles: ".sem-evento{color: #fff;} #futebol-default-wrapper, #futebol-live-wrapper, #jogo-default-wrapper{ background: #0f0f11!important } .jogos, .eventos{ background: #0f0f11!important; color: #fff!important; } .jogo{ border-top: none!important; } .campeonato-header{ border-bottom-color: #0f0f11!important; } .footer{ background-color:#0f0f11!important; }" +
-            ".jogo-container .indentificacao-mobile, .jogo-container .indentificacao, .jogo-container .campeonato-nome{background: #e1e2e4;} .indentificacao .tempo{color: #fff!important;} .inside-event .tipo-apostas{ background-color: #0f0f11!important; color: white!important; } #esportes-footer{ color:white; } ",
-        scripts: "<!--Start of Tawk.to Script--> <script type='text/javascript'> var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); (function(){ var s1=document.createElement('script'),s0=document.getElementsByTagName('script')[0]; s1.async=true; s1.src='https://embed.tawk.to/5f6b3ee6f0e7167d001303f3/default'; s1.charset='UTF-8'; s1.setAttribute('crossorigin','*'); s0.parentNode.insertBefore(s1,s0); })(); </script> <!--End of Tawk.to Script-->"+
-        "<!-- Google tag (gtag.js) --><script async src='https://www.googletagmanager.com/gtag/js?id=G-V8VVFKES6F'></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-V8VVFKES6F');</script>"
-    });
-});
-
 gulp.task('slsports.wee.bet', function (done) {
     tasks(done, {
         server: "front1.wee.bet",
