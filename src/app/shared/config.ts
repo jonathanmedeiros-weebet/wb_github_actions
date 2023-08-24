8a179dcf5940ed1ad062d25d82db648ee98d57f7
David Silva <david.silva@lti.net.br>
2023-08-24 11:32:21 -0300

    correções

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,4 +1,4 @@
-const _host = 'http://192.168.0.105';
+const _host = '//weebet.local';
 // const _host = '//central.demo.wee.bet';
 // const _center = '//localhost:3002';
 const _center = 'https://hermes.wee.bet';
@@ -12,14 +12,14 @@ export const config: any = {
     TIMESTAMP: _timestamp,
     HOST: _host,
     BASE_URL: `${_host}/api`,
-    SHARED_URL: '//weebet.david',
+    SHARED_URL: '//weebet.local',
     CENTER_HOST: _center,
     LIVE_HOST: _live,
     STATS_HOST: _stats,
     CENTER_API: `${_center}/v1`,
     SPORTS_URL: `${_host}/api/esportes`,
     LOTTERIES_URL: `${_host}/api/loteria`,
-    SLUG: 'weebet.david',
+    SLUG: 'demo.wee.bet',
     // LOGO: _host.replace('central.', '')+'/assets/images/logo_banca.png',
     LOGO: 'https://weebet.s3.amazonaws.com/demo.wee.bet/logos/logo_banca.png',
 
