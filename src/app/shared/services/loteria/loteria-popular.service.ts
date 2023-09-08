e0381734f657ecc774a3c1398ed1ebd5cb4a63af
Matheus Amorim <matheus@lti.net.br>
2023-09-08 18:32:46 -0300

    adicionada tab loteria popular no relatorio de apostas

--- src/app/shared/services/loteria/loteria-popular.service.ts
+++ src/app/shared/services/loteria/loteria-popular.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
 import { HeadersService } from '../utils/headers.service';
@@ -26,6 +27,22 @@ export class LoteriaPopularService {
                 map((res: any) => {
                     return res;
                 }),
+                catchError(this.errorService.handleError)
+            );
+    }
+
+    getApostas(queryParams?: any): Observable<any> {
+        let requestOptions;
+
+        if (queryParams) {
+            requestOptions = this.header.getRequestOptions(true, queryParams);
+        } else {
+            requestOptions = this.header.getRequestOptions(true);
+        }
+
+        return this.http.get(`${this.loteria_popular_url}/bets`, requestOptions)
+            .pipe(
+                map((res: any) => res.results),
                 catchError(this.errorService.handleError)
             );
     }
