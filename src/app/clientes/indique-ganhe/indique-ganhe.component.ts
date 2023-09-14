d7fa28844a2a9554006c260a1602887e620f460b
Matheus Amorim <matheus@lti.net.br>
2023-09-14 19:27:52 -0300

    Wip indique e ganhe. Estilizacoes, modal de regras e botoes de link e compartilhamento

--- src/app/clientes/indique-ganhe/indique-ganhe.component.ts
+++ src/app/clientes/indique-ganhe/indique-ganhe.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
+import { config } from '../../shared/config';
 
 import { ClienteService, MessageService, ParametrosLocaisService, SidebarService } from 'src/app/services';
 
@@ -8,15 +11,26 @@ import { ClienteService, MessageService, ParametrosLocaisService, SidebarService
   styleUrls: ['./indique-ganhe.component.css']
 })
 export class IndiqueGanheComponent implements OnInit {
+    @ViewChild('regrasCondicoesModal', {static: true}) regrasCondicoesModal;
     linkIndicacao;
     valorGanhoPorIndicacao;
+
+    linkFacebook;
+    linkWhatsapp;
+    linkTelegram;
+    linkEmail;
+    linkInstagram;
+
     mobileScreen;
 
     constructor(
+        private activeModal: NgbActiveModal,
         private clienteService: ClienteService,
         private messageService: MessageService,
+        private modalService: NgbModal,
         private paramsLocaisService: ParametrosLocaisService,
-        private sidebarService: SidebarService
+        private sidebarService: SidebarService,
+        private translateService: TranslateService
     ) { }
 
     ngOnInit(): void {
@@ -26,7 +40,12 @@ export class IndiqueGanheComponent implements OnInit {
         this.clienteService.getLinkIndicacao()
             .subscribe(
                 response => {
-                    this.linkIndicacao = response.linkIndicacao
+                    this.linkIndicacao = response.linkIndicacao;
+                    let urlEncodedLinkIndicacao = response.linkIndicacao.replace(/\//g, "%2F").replace(/\:/g, "%3A").replace(/\?/g, "%3F").replace(/\=/, "%3D");
+                    this.linkFacebook = `https://www.facebook.com/sharer/sharer.php?u=${urlEncodedLinkIndicacao}`;
+                    this.linkWhatsapp = `https://api.whatsapp.com/send/?text=${urlEncodedLinkIndicacao}&type=custom_url&app_absent=0`;
+                    this.linkTelegram = `https://telegram.me/share/url?url=${urlEncodedLinkIndicacao}&text=Cadastre-se%20na%20${config.BANCA_NOME}%21`;
+                    this.linkEmail = `mailto:info@example.com?&subject=&cc=&bcc=&body=${urlEncodedLinkIndicacao}%0A`;
                 },
                 error => {
                     this.messageService.error(error);
@@ -36,5 +55,21 @@ export class IndiqueGanheComponent implements OnInit {
         if (!this.mobileScreen) {
             this.sidebarService.changeItens({contexto: 'cliente'});
         }
+    }
+
+    abrirModalRegras()
+    {
+        this.activeModal = this.modalService.open(
+            this.regrasCondicoesModal,
+            {
+                ariaLabelledBy: 'modal-basic-title',
+                centered: true
+            }
+        );
+    }
+
+    copiarLink() {
+        navigator.clipboard.writeText(this.linkIndicacao);
+        this.messageService.success(this.translateService.instant('geral.linkCopiado'));
     }
 }
