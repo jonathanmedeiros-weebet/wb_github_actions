5af00623725f0b711dcbda1baf7cf3c283b77f38
Thiago Faustino <thiago.faustino@lti.net.br>
2023-10-26 19:35:32 -0300

    troca de dominio

--- gulpfile.js
+++ gulpfile.js
@@ -2844,14 +2844,12 @@ gulp.task('suasortebet.com', function (done) {
     });
 });
 
-gulp.task('betsstarks.com', function (done) {
+gulp.task('brabet.app', function (done) {
     tasks(done, {
         server: "front5.wee.bet",
-        host: "betsstarks.com",
-        banca: "BETS STARKS",
-        styles: "",
-        pixel: "<link rel='stylesheet' href='https://cdn.wee.bet/jivosite/jivosite.css'>",
-        scripts: "<script src='https://cdn.wee.bet/jivosite/jivosite.js' type='text/javascript'></script><script src='//code.jivosite.com/widget/bwW3zlaKcy' async></script>",
+        host: "brabet.app",
+        banca: "BRABET",
+        styles: ""
     });
 });
 
