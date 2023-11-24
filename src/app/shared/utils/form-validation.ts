03490bdf0d9ee4bf435c08918d4169dade71590a
Emerson Silva <emerson.silva@lti.net.br>
2023-11-24 17:18:06 -0300

    removendo import desnecessario

--- src/app/shared/utils/form-validation.ts
+++ src/app/shared/utils/form-validation.ts
@@ -1,4 +1,4 @@
-import {UntypedFormArray, UntypedFormControl, UntypedFormGroup, Validators} from '@angular/forms';
+import {UntypedFormArray, UntypedFormControl, UntypedFormGroup} from '@angular/forms';
 import * as moment from 'moment';
 
 export class FormValidations {
