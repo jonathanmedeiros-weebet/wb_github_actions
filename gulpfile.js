d986bf9d2cad0fd37a6bc192be6f657e3d8cf606
Gustavo Andrade <gustavoandradedeveloper@gmail.com>
2023-11-29 09:34:29 -0300

    Conforme solicitado removi o pixel que o cliente brinkbet.com pediu

--- gulpfile.js
+++ gulpfile.js
@@ -2329,7 +2329,7 @@ gulp.task('brinkbet.com', function (done) {
         host: "brinkbet.com",
         banca: "BRINK BET",
         styles: "",
-        pixel:"<!-- Meta Pixel Code --><script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '3511728055748794');fbq('track', 'PageView');</script><noscript><img height='1' width='1' style='display:none'src='https://www.facebook.com/tr?id=3511728055748794&ev=PageView&noscript=1'/></noscript><!-- End Meta Pixel Code -->",
+        pixel:"",
     });
 });
 
