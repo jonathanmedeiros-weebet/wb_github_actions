26d282dfc60ed6d81091cf7bbf1ece5b5b2712be
Matheus Amorim <matheus@lti.net.br>
2024-02-20 14:41:39 -0300

    Processo de login pela Loki api

--- src/app/shared/layout/modals/login-modal/login-modal.component.ts
+++ src/app/shared/layout/modals/login-modal/login-modal.component.ts
@@ -123,8 +123,8 @@ export class LoginModalComponent extends BaseFormComponent implements OnInit, On
                         this.auth.getCookie(this.usuario.cookie) === '' &&
                         this.usuario.login !== 'weebet') {
                         this.abrirModalAuthDoisFatores();
-                    } else if (res && res.migracao) {
-                        this.router.navigate([`/auth/resetar-senha/${res.migracao.token}/${res.migracao.codigo}`]);
+                    } else if (res && res.results && res.results.migracao) {
+                        this.router.navigate([`/auth/resetar-senha/${res.results.migracao.token}/${res.results.migracao.codigo}`]);
                         this.activeModal.dismiss();
                     } else {
                         this.form.value.cookie = this.auth.getCookie(this.usuario.cookie);
