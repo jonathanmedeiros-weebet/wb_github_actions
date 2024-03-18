df8b7579611e3ee54977b7c7a255f17444b66492
Thiago Faustino <thiago.faustino@lti.net.br>
2024-03-18 11:15:23 -0300

    remove

--- gulpfile.js
+++ gulpfile.js
@@ -109,15 +109,6 @@ gulp.task('demo.wee.bet', function (done) {
     });
 });
 
-gulp.task('vitrine.wee.bet', function (done) {
-    tasks(done, {
-        server: "front1.wee.bet",
-        host: "vitrine.wee.bet",
-        banca: "Vitrine",
-        styles: "--header: #222d32; --foreground-header: #35cc96; --sidebar-right:#1a2226; --foreground-sidebar-right: #fff; --sidebar-left: #1a2226; --foreground-sidebar-left: #fff; --highlight: #35cc96; --foreground-highlight: #d2d6de; --odds: #2c3b41; --foreground-odds: #fff;--foreground-selected-odds: #fff;--selected-event: #1e282c;--event-time: #1e282c;",
-    });
-});
-
 gulp.task('sandbox.wee.bet', function (done) {
     tasks(done, {
         server: "front4.wee.bet",
