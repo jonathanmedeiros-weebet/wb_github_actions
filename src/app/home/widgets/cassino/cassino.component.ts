b6dfca687feb194024546a42765bcbbc019d87db
Iuri Brito <iuri.brito@lti.net.br>
2024-04-04 18:24:23 -0300

    widgets da home

--- src/app/home/widgets/cassino/cassino.component.ts
+++ src/app/home/widgets/cassino/cassino.component.ts
@@ -1,10 +1,113 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
+import { AuthService, ParametrosLocaisService, SidebarService } from 'src/app/services';
+import { LoginModalComponent } from 'src/app/shared/layout/modals';
+import { CasinoApiService } from 'src/app/shared/services/casino/casino-api.service';
 
 @Component({
-  selector: 'app-cassino',
-  templateUrl: './cassino.component.html',
-  styleUrls: ['./cassino.component.css']
+    selector: 'app-cassino',
+    templateUrl: './cassino.component.html',
+    styleUrls: ['./cassino.component.css']
 })
-export class CassinoComponent {
+export class CassinoComponent implements OnInit {
+    gamesCassino = [];
+    gamesDestaque = [];
 
+    isMobile = false;
+    scrolls: ElementRef[];
+    qtdItens = 0;
+    modalRef;
+
+    constructor(
+        private casinoApi: CasinoApiService,
+        private auth: AuthService,
+        private route: ActivatedRoute,
+        private router: Router,
+        private modalService: NgbModal,
+        private sideBarService: SidebarService,
+        private renderer: Renderer2,
+        private el: ElementRef,
+        private cd: ChangeDetectorRef,
+        private translate: TranslateService,
+        private paramsService: ParametrosLocaisService
+    ) { }
+
+    ngOnInit(): void {
+        this.casinoApi.getGamesList(false).subscribe(response => {
+            this.gamesCassino = response.gameList.filter(function(game) {
+                return game.dataType !== 'VSB';
+            });
+            this.gamesDestaque = response.populares;
+        }, erro => { });
+    }
+
+    scrollLeft(scrollId) {
+        const scrollTemp = this.scrolls.find((scroll) => scroll.nativeElement.id === scrollId + '-scroll');
+        scrollTemp.nativeElement.scrollLeft -= 700;
+    }
+
+    scrollRight(scrollId) {
+        const scrollTemp = this.scrolls.find((scroll) => scroll.nativeElement.id === scrollId + '-scroll');
+        scrollTemp.nativeElement.scrollLeft += 700;
+    }
+
+    onScroll(scrollId) {
+        this.cd.detectChanges();
+        const scrollTemp = this.scrolls.find((scroll) => scroll.nativeElement.id === scrollId + '-scroll');
+        const scrollLeft = scrollTemp.nativeElement.scrollLeft;
+        const scrollWidth = scrollTemp.nativeElement.scrollWidth;
+
+        const scrollLeftTemp = this.el.nativeElement.querySelector(`#${scrollId}-left`);
+        const scrollRightTemp = this.el.nativeElement.querySelector(`#${scrollId}-right`);
+
+        const fadeLeftTemp = this.el.nativeElement.querySelector(`#${scrollId}-fade-left`);
+        const fadeRightTemp = this.el.nativeElement.querySelector(`#${scrollId}-fade-right`);
+
+        const maxScrollSize = scrollTemp.nativeElement.clientWidth;
+
+        if (scrollLeft <= 0) {
+            if (!this.isMobile) {
+                this.renderer.addClass(scrollLeftTemp, 'disabled-scroll-button');
+                this.renderer.removeClass(scrollLeftTemp, 'enabled-scroll-button');
+            }
+            this.renderer.setStyle(fadeLeftTemp, 'opacity', '0');
+        } else {
+            if (!this.isMobile) {
+                this.renderer.addClass(scrollLeftTemp, 'enabled-scroll-button');
+                this.renderer.removeClass(scrollLeftTemp, 'disabled-scroll-button');
+            }
+            this.renderer.setStyle(fadeLeftTemp, 'opacity', '1');
+        }
+
+        if ((scrollWidth - (scrollLeft + maxScrollSize)) <= 1) {
+            if (!this.isMobile) {
+                this.renderer.addClass(scrollRightTemp, 'disabled-scroll-button');
+                this.renderer.removeClass(scrollRightTemp, 'enabled-scroll-button');
+            }
+            this.renderer.setStyle(fadeRightTemp, 'opacity', '0');
+        } else {
+            if (!this.isMobile) {
+                this.renderer.addClass(scrollRightTemp, 'enabled-scroll-button');
+                this.renderer.removeClass(scrollRightTemp, 'disabled-scroll-button');
+            }
+            this.renderer.setStyle(fadeRightTemp, 'opacity', '1');
+        }
+    }
+
+    abrirModalLogin() {
+        this.modalRef = this.modalService.open(
+            LoginModalComponent,
+            {
+                ariaLabelledBy: 'modal-basic-title',
+                windowClass: 'modal-550 modal-h-350 modal-login',
+                centered: true,
+            }
+        );
+    }
+
+    exibirMais() {
+        this.qtdItens += 3;
+    }
 }
--- src/app/home/widgets/jogos-destaque/jogos-destaque.component.ts
+++ src/app/home/widgets/jogos-destaque/jogos-destaque.component.ts
@@ -1,10 +1,171 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output, OnChanges, SimpleChanges } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { BilheteEsportivoService, HelperService, JogoService } from 'src/app/services';
 
 @Component({
-  selector: 'app-jogos-destaque',
-  templateUrl: './jogos-destaque.component.html',
-  styleUrls: ['./jogos-destaque.component.css']
+    selector: 'app-jogos-destaque',
+    templateUrl: './jogos-destaque.component.html',
+    styleUrls: ['./jogos-destaque.component.css']
 })
-export class JogosDestaqueComponent {
+export class JogosDestaqueComponent implements OnInit, OnChanges {
+    @Output() maisCotacoesDestaque = new EventEmitter();
+    @Input() jogoIdAtual;
+    jogosDestaque = [];
+    regioesDestaque = null;
+    exibindoRegiao = false;
+    exibirDestaques = false;
+    menuWidth;
+    mobileScreen;
+    itens = [];
+    itensSelecionados = {};
+    isDragging = false;
 
+    jogosDestaquesIds = [];
+
+    customOptions: OwlOptions = {
+        loop: false,
+        autoplay: true,
+        rewind: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        autoHeight: true,
+        autoWidth: true,
+        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>']
+    };
+
+    unsub$ = new Subject();
+
+    constructor(
+        private jogoService: JogoService,
+        private helperService: HelperService,
+        private cd: ChangeDetectorRef,
+        private bilheteService: BilheteEsportivoService,
+    ) { }
+
+    ngOnInit() {
+        this.mobileScreen = window.innerWidth <= 1024 ? true : false;
+
+        // Recebendo os itens atuais do bilhete
+        this.bilheteService.itensAtuais
+            .pipe(takeUntil(this.unsub$))
+            .subscribe(itens => {
+                this.itens = itens;
+
+                this.itensSelecionados = {};
+                for (let i = 0; i < itens.length; i++) {
+                    const item = itens[i];
+                    this.itensSelecionados[`${item.jogo_id}_${item.cotacao.chave}`] = true;
+                }
+
+                this.cd.markForCheck();
+            });
+
+        this.getJogosDestaques();
+        this.remapJogosDestaque();
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['jogosDestaque']) {
+            this.remapJogosDestaque();
+        }
+    }
+
+    getJogosDestaques() {
+        this.jogoService.getJogosDestaque()
+            .subscribe(jogos => {
+                this.jogosDestaque = jogos.results;
+                this.jogosDestaquesIds = jogos.results.map(jogo => jogo.fi + '');
+                // this.mapJogosDestaque();
+                console.log('JOGOS:', this.jogosDestaque);
+            });
+    }
+
+    // mapJogosDestaque() {
+    //     let jogosDestaques = [];
+    //
+    //     if (this.camps && this.camps.length > 0) {
+    //         this.camps.forEach(camp => {
+    //             const jogosSele = camp.jogos.filter(jogo => {
+    //                 return this.jogosDestaquesIds.includes(jogo._id + '');
+    //             });
+    //
+    //             jogosDestaques = jogosDestaques.concat(jogosSele);
+    //         });
+    //     }
+    //
+    //     this.jogosDestaque = jogosDestaques;
+    //     this.cd.detectChanges();
+    // }
+
+    remapJogosDestaque() {
+        this.jogosDestaque.forEach((jogo) => {
+            jogo.cotacoes.slice(0, 3).forEach((cotacao) => {
+                if (!cotacao.valorFinal) {
+                    cotacao.valorFinal = this.helperService.calcularCotacao2String(
+                        cotacao.valor,
+                        cotacao.chave,
+                        jogo.event_id,
+                        jogo.favorito,
+                        false);
+                }
+            });
+        });
+        this.cd.detectChanges();
+    }
+
+    cotacaoPermitida(cotacao) {
+        return this.helperService.cotacaoPermitida(cotacao);
+    }
+
+    maisCotacoes(jogoId) {
+        if (!this.isDragging) {
+            this.jogoIdAtual = jogoId;
+            this.maisCotacoesDestaque.emit(jogoId);
+        }
+    }
+
+    addCotacao(event, jogo, cotacao) {
+        event.stopPropagation();
+
+        if (!this.isDragging) {
+            let modificado = false;
+            const indexGame = this.itens.findIndex(i => i.jogo._id === jogo._id);
+            const indexOdd = this.itens.findIndex(i => (i.jogo._id === jogo._id) && (i.cotacao.chave === cotacao.chave));
+
+            const item = {
+                ao_vivo: jogo.ao_vivo,
+                jogo_id: jogo._id,
+                jogo_event_id: jogo.event_id,
+                jogo_nome: jogo.nome,
+                cotacao: {
+                    chave: cotacao.chave,
+                    valor: cotacao.valor,
+                    nome: cotacao.nome
+                },
+                jogo: jogo,
+                mudanca: false,
+                cotacao_antiga_valor: null
+            };
+
+            if (indexGame >= 0) {
+                if (indexOdd >= 0) {
+                    this.itens.splice(indexOdd, 1);
+                } else {
+                    this.itens.splice(indexGame, 1, item);
+                }
+
+                modificado = true;
+            } else {
+                this.itens.push(item);
+                modificado = true;
+            }
+
+            if (modificado) {
+                this.bilheteService.atualizarItens(this.itens);
+            }
+        }
+    }
 }
