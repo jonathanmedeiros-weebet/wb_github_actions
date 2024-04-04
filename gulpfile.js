4d4760beeecfb1e73ac7a69cedaa80d78c8d8aa3
Thiago Faustino <thiago.faustino@lti.net.br>
2024-04-04 18:13:16 -0300

    ajuste jbnacional

--- gulpfile.js
+++ gulpfile.js
@@ -2375,7 +2375,7 @@ gulp.task('lottomaster.bet', function (done) {
 
 gulp.task('jbnacional.bet', function (done) {
     tasks(done, {
-        server: "front5.wee.bet",
+        server: "front1.wee.bet",
         host: "jbnacional.bet",
         banca: "JB NACIONAL",
         styles: "",
