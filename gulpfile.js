8fc70500fb988c89354cc1b070b5f6490ad997d0
Matheus Amorim <matheus@lti.net.br>
2024-04-30 10:34:29 -0300

    ajuste no gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1679,6 +1679,7 @@ gulp.task('nacionalbet.club', function (done) {
         host: "nacionalbet.club",
         banca: "NACIONAL BET",
         styles: "",
+        description: "A melhor maneira de fazer apostas online nos seus esportes favoritos e ganhar até R$3000 em bônus ao se registrar. Aproveite a experiência da NacionalBET e as melhores ODDS. Site de apostas 100% confiável. Faça sua aposta!",
     });
 });
 
@@ -1689,7 +1690,6 @@ gulp.task('iasports.wee.bet', function (done) {
         shared_url: "iasport.bet",
         banca: "IA SPORTS",
         styles: "",
-        description: "A melhor maneira de fazer apostas online nos seus esportes favoritos e ganhar até R$3000 em bônus ao se registrar. Aproveite a experiência da NacionalBET e as melhores ODDS. Site de apostas 100% confiável. Faça sua aposta!",
     });
 });
 
