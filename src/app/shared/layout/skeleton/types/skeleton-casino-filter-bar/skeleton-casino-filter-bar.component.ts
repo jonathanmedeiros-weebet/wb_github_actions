9cc1cc23cc713857fe49cb34b95a235eb4232bc8
Jonathan Tifinancias <jonathantifinancias@gmail.com>
2024-05-03 11:50:20 -0300

    feat(skeleton): criação e uso do component skeleton na pagina home e cassino

---
+++ src/app/shared/layout/skeleton/types/skeleton-casino-filter-bar/skeleton-casino-filter-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-casino-filter-bar',
+  templateUrl: './skeleton-casino-filter-bar.component.html',
+  styleUrls: ['./skeleton-casino-filter-bar.component.css']
+})
+export class SkeletonCasinoFilterBarComponent {
+
+}
---
+++ src/app/shared/layout/skeleton/types/skeleton-casino/skeleton-casino.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-casino',
+  templateUrl: './skeleton-casino.component.html',
+  styleUrls: ['./skeleton-casino.component.css']
+})
+export class SkeletonCasinoComponent {}
