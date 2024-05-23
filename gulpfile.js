f926b59d755ee446e2d78861701290c2666f6842
Thiago Faustino <thiago.faustino@lti.net.br>
2024-05-23 00:14:40 -0300

    remove

--- gulpfile.js
+++ gulpfile.js
@@ -1591,19 +1591,6 @@ gulp.task('cassinoaovivo.bet', function (done) {
     });
 });
 
-gulp.task('blackpix.bet', function (done) {
-    tasks(done, {
-        server: "front2.wee.bet",
-        host: "blackpix.bet",
-        banca: "BlackPix.Bet | Online Cassino",
-        styles: "",
-        google_tag_part_1: "<!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TBZK44G');</script><!-- End Google Tag Manager -->",
-        google_tag_part_2: "<!-- Google Tag Manager (noscript) --><noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-TBZK44G'height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript><!-- End Google Tag Manager (noscript) -->",
-        pixel: "<link rel='stylesheet' href='https://cdn.wee.bet/jivosite/jivosite.css'>",
-        scripts: "<script src='https://cdn.wee.bet/jivosite/jivosite.js' type='text/javascript'></script><script src='//code.jivosite.com/widget/F3liZ9TarL' async></script>"
-    });
-});
-
 gulp.task('n7futmix.com', function (done) {
     tasks(done, {
         server: "front1.wee.bet",
