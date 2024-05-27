cb8aa81edfd00f36cd9bd3972a2e5b3acf0015e3
Jonathan Tifinancias <jonathantifinancias@gmail.com>
2024-05-27 15:10:35 -0300

    feat: migração do mapa de ultimos acessos

---
+++ src/app/shared/services/geolocation.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+
+export interface Geolocation {
+    error: boolean;
+    lat: number;
+    lng: number;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GeolocationService {
+    async getGeolocation(): Promise<Geolocation> {
+        try {
+            return await this.getCurrentPosition() as Geolocation;
+        } catch (error) {
+            return {
+                error: true,
+                lat: 0,
+                lng: 0
+            }
+        }
+    }
+
+    private getCurrentPosition() {
+        return new Promise((resolve, reject) => {
+            try {
+                if (!navigator.geolocation) throw new Error('Geolocation not allowed.');
+                
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        if (!position) throw new Error('Unable to find current position')
+    
+                        resolve({
+                            error: false,
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        });
+                    },
+                    (error) => {
+                        throw error
+                    }
+                );
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+}
