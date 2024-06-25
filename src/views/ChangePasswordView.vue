6d3c2cf6ba00f6f849f324f127b267645263f6cf
Emanuel Aguiar <emanuel_pe@live.com>
2024-06-25 17:01:17 -0300

    refactor: ajustes na identação e classes css

--- src/views/ChangePasswordView.vue
+++ src/views/ChangePasswordView.vue
@@ -1,49 +1,44 @@
 <template>
-    <Header 
-    title="Alterar senha"
-    :showBackButton="true" 
-    />
     <div class="password">
-
+      <Header title="Alterar senha" :showBackButton="true" />
         <div class="password__container">
+          <div class="password_inputs">
+            <w-input
+            class="login__input"
+            label="Senha atual"
+            name="current_password"
+            placeholder="Digite sua senha atual"
+            type="password"
+            >
+            </w-input>
+            
+            <w-input
+            class="login__input"
+            label="Nova Senha"
+            name="new_password"
+            placeholder="Digite sua nova senha"
+            type="password"
+            >
+            </w-input>
 
-            <div class="password_inputs">
-                <w-input
-                class="login__input"
-                label="Senha atual"
-                name="current_password"
-                placeholder="Digite sua senha atual"
-                type="password"
-                >
-                </w-input>
-                
-                <w-input
-                class="login__input"
-                label="Nova Senha"
-                name="new_password"
-                placeholder="Digite sua nova senha"
-                type="password"
-                >
-                </w-input>
-    
-                <w-input
-                class="login__input"
-                label="Confirmar Senha"
-                name="confirm_password"
-                placeholder="Confirme a nova senha"
-                type="password"
-                >
-                </w-input>
-            </div>
+            <w-input
+            class="login__input"
+            label="Confirmar Senha"
+            name="confirm_password"
+            placeholder="Confirme a nova senha"
+            type="password"
+            >
+            </w-input>
+          </div>
 
-        </div>
-        <div class="password__button">
+          <div class="password__button">
             <w-button
             id="btn-salvar"
             text="Salvar Alterações"
             value="salvar"
             name="btn-salvar"
             />
+          </div>
         </div>
     </div>
 </template>
@@ -70,26 +65,23 @@ export default {
 .password {
     display: flex;
     flex-direction: column;
-    height: 90vh;
-    padding: 0px 30px;
+    height: 100vh;
   
   &__container {
     flex: 1;
     display: flex;
     flex-direction: column;
-    padding-top: 20px;
-    
+    justify-content: space-between;
+    padding: 20px 30px 0px 30px;
   }
 
   &__inputs {
-    flex: 1;
     display: flex;
     flex-direction: column;
-    
   }
 
   &__button {
-    align-items: flex-end;
+    justify-content: center;
     margin-bottom: 20px;
   }
 }
