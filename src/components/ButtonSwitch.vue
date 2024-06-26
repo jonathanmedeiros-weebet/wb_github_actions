710cdeedd7aad50ed6428af895d8432c2363e358
Emanuel Aguiar <emanuel_pe@live.com>
2024-06-26 14:01:08 -0300

    feat: criação da tela de configurações

---
+++ src/components/ButtonSwitch.vue
@@ -0,0 +1,82 @@
+<template>
+    <label class="toggle">
+        <input 
+          class="toggle__checkbox" 
+          type="checkbox"
+          :checked="isChecked" 
+          @change="toggleCheckbox"
+        >
+        <div class="toggle__switch"></div>
+    </label>
+</template>
+
+<script>
+export default {
+    name: 'button-switch',
+    props: {
+    isChecked: {
+      type: Boolean,
+      required: true
+    },
+    methods: {
+        toggleCheckbox(event) {
+            this.$emit('update:isChecked', event.target.checked);
+        }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.toggle {
+  cursor: pointer;
+  display: inline-block;
+
+  &__switch {
+    display: inline-block;
+    background: #ccc;
+    border-radius: 16px;
+    width: 39px;
+    height: 20px;
+    position: relative;
+    vertical-align: middle;
+    transition: background 0.25s;
+  }
+
+  &__switch:before, &__switch:after {
+    content: "";
+  }
+
+  &__switch:before {
+    display: block;
+    background: linear-gradient(to bottom, #fff 0%, #eee 100%);
+    border-radius: 50%;
+    width: 14px;
+    height: 14px;
+    position: absolute;
+    top: 3px;
+    left: 3px;
+    transition: left 0.25s;
+  }
+
+  &__checkbox:checked + &__switch {
+    background: rgba(109, 165, 68, 1);
+  }
+
+  &__checkbox:checked + &__switch:before {
+    left: 22px;
+  }
+
+  &__checkbox {
+    position: absolute;
+    visibility: hidden;
+  }
+
+  &:hover &__switch:before {
+    background: linear-gradient(to bottom, #fff 0%, #fff 100%);
+  }
+}
+
+
+
+</style>
---
+++ src/views/ConfigView.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="config">
+        <Header title="Configurações" :showBackButton="true" />
+        <div class="config_container">
+            <div class="config_content">
+                <p class="config__title">Ativiar bluetooh</p>
+                <button-switch :isChecked="bluetooh" />
+            </div>
+            <div class="config_content_column">
+                <p class="config__title">Impressora padrão</p>
+                <p class="config_description">Selecionar um modelo de impressora abaixo</p>
+            </div>
+            <div class="config_content">
+                <p class="config__title">Imprimir elementos gráficos</p>
+                <button-switch :isChecked="bluetooh" />
+            </div>
+        </div>
+  </div>
+    
+</template>
+
+<script>
+import Header from '@/components/layouts/Header.vue'
+import ButtonSwitch from '@/components/ButtonSwitch.vue'
+
+export default {
+    name: 'config',
+    components: { 
+        Header,
+        ButtonSwitch
+    },
+    data() {
+        return {
+            bluetooh: true,
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.config {
+    
+    &_container {
+        padding-top: 30px;
+        align-items: center;
+        padding: 30px 30px 0px 30px;
+    }
+
+    &_content {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-bottom: 20px;
+    }
+
+    &__title {
+        margin: 0;
+        font-size: 16px;
+        font-weight: 400;
+        
+    }
+
+    &_content_column {
+        display: flex;
+        flex-direction: column;
+        
+
+    }
+
+    &_description {
+        font-size: 14px;
+        color: var(--color-text-input);
+        margin-bottom: 20px;
+    }
+}
+</style>
