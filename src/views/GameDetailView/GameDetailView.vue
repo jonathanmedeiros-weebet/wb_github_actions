145211793c7171f28a3c7f1421b6d3898507d0d6
Jonathan Tifinancias <jonathantifinancias@gmail.com>
2024-07-01 17:19:05 -0300

    feat: pagina de detalhe do jpogo

---
+++ src/views/GameDetailView/GameDetailView.vue
@@ -0,0 +1,204 @@
+<template>
+    <div class="game-detail">
+        <div class="game-detail__header">
+           <GameDetailHeader :game="game" type="slim"/>
+        </div>
+        <div class="game-detail__body">
+           <div class="game-detail__filters">
+                <button
+                    class="game-detail__filter"
+                    v-for="({ title, selected, slug}, index) in filters"
+                    :key="index"
+                    :class="{'game-detail__filter--selected': selected}"
+                    @click="handleGameFilter(slug)"
+                >
+                    {{ title }}
+                </button>
+            </div>
+            <Collapse
+                :initCollapsed="true"
+                v-for="({group, bets}, index) in options"
+                :key="index"
+            > 
+                <template #title>{{ group }}</template>
+
+                <div class="collapse__options" :class="{'collapse__options--grid': bets.length > 3}">
+                    <button
+                        class="collapse__option"
+                        v-for="({label, value}, betIndex) in bets"
+                        :key="`${betIndex}-${index}`"
+                        :class="{'collapse__option--selected': betIndex === 0}"
+                        @click="handleItemClick()"
+                    >
+                        <span class="collapse__label">{{ label }}</span>
+                        <span class="collapse__value">{{ value }}</span>
+                    </button>
+                </div>
+            </Collapse>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/layouts/Header.vue'
+import { gameBetList, gameList } from '@/constants'
+import GameDetailHeader from './parts/GameDetailHeader.vue'
+import { GameDetailFilter } from '@/enums'
+import Collapse from '@/components/Collapse.vue'
+import Button from '@/components/Button.vue'
+
+export default {
+  components: { Header, GameDetailHeader, Collapse, Button },
+    name: 'game-detail',
+    data() {
+        return {
+            headerFixed: true,
+            game: gameList[0],
+            filterSelected: GameDetailFilter.FULL_TIME
+        }
+    },
+    computed: {
+        filters() {
+            return [
+                {
+                    title: 'Tempo completo',
+                    selected: this.filterSelected === GameDetailFilter.FULL_TIME,
+                    slug: GameDetailFilter.FULL_TIME
+                },
+                {
+                    title: 'Primeiro tempo',
+                    selected: this.filterSelected === GameDetailFilter.FIRST_TIME,
+                    slug: GameDetailFilter.FIRST_TIME
+                },
+                {
+                    title: 'Segundo tempo',
+                    selected: this.filterSelected === GameDetailFilter.SECOND_TIME,
+                    slug: GameDetailFilter.SECOND_TIME
+                }
+            ]
+        },
+        options() {
+            return gameBetList;
+        }
+    },
+    methods: {
+        handleGameFilter(filter) {
+            this.filterSelected = filter
+        },
+        handleItemClick() {
+            event.stopPropagation();
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.game-detail {
+    padding-bottom: 20px;
+
+    &__filters {
+        height: 53px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 10px;
+        padding: 0 10px;
+
+        width: 100%;
+        overflow-y: auto;
+        -ms-overflow-style: none;  /* Internet Explorer 10+ */
+        scrollbar-width: none;  /* Firefox */
+
+        border-bottom: 1px solid #ffffff1a;
+        background: var(--color-background-input);
+    }
+
+    &__filters::-webkit-scrollbar {
+        display: none;  /* WebKit-based browsers */
+    }
+
+    &__filter {
+        height: 100%;
+        font-size: 16px;
+        font-weight: 400;
+        line-height: normal;
+
+        background: transparent;
+        border: none;
+        white-space: nowrap;
+
+        color: #ffffff80;
+        border-bottom: 3px solid transparent;
+        &--selected {
+            color: #0AAF6D;
+            border-color: #0AAF6D;
+        }
+    }
+}
+
+.collapse {
+    &__options {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+        gap: 10px;
+
+        padding: 13px 16px;
+
+        background: var(--color-background-input);
+
+        &--grid {
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr;
+        }
+    }
+
+    &__option {
+        height: 54px;
+        width: 100%;
+        background: var(--color-background);
+        border: none;
+        border-radius: 4px;
+        padding: 9px 15px;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 7px;
+
+        &--selected {
+            background: var(--color-primary);
+        }
+    }
+
+    &__option--selected &__label,
+    &__option--selected &__value {
+        color: #000;
+    }
+
+    &__label {
+        overflow: hidden;
+        color: #f2f2f280;
+        text-overflow: ellipsis;
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+    }
+
+    &__value {
+        color: var(--color-text);
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: normal;
+    }
+}
+
+::v-deep .collapse__item {
+    background: var(--color-background);
+    padding: 13px 24px;
+}
+
+</style>
---
+++ src/views/GameDetailView/parts/GameDetailHeader.vue
@@ -0,0 +1,208 @@
+<template>
+        <div
+            class="game-detail-header"
+            :class="{
+                'game-detail-header--slim': type === 'slim',
+                'game-detail-header--fixed': fixed,
+            }"
+        >
+            <div class="background" :class="{'background--slim': type === 'slim'}">
+                <div class="background__image" :style="{'backgroundImage': `url(${teamA.image})`}"></div>
+                <div class="background__image" :style="{'backgroundImage': `url(${teamB.image})`}"></div>
+            </div>
+            
+            <div class="game-detail-header__container">
+                <Header :showBackButton="true">
+                    <span class="game-detail-header__title" v-if="isHeaderNormal">
+                        <span> Campeonato brasileiro </span>
+                        <span> {{ game.dateTime}} </span>
+                    </span>
+
+                    <div class="game-detail-header__teams game-detail-header__teams--slim" v-if="isHeaderSlim">
+                        <div class="team">
+                            <img class="team__image" :src="teamA.image">
+                        </div>
+                        <span class="team__versus">X</span>
+                        <div class="team">
+                            <img class="team__image" :src="teamB.image">
+                        </div>
+                    </div>
+
+                </Header>
+
+                <div class="game-detail-header__teams" v-if="isHeaderNormal">
+                    <div class="team">
+                        <img class="team__image" :src="teamA.image">
+                        <span class="team__name">{{ teamA.name }}</span>
+                    </div>
+                    <span class="team__versus">X</span>
+                    <div class="team">
+                        <img class="team__image" :src="teamB.image">
+                        <span class="team__name">{{ teamB.name }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+import Header from '@/components/layouts/Header.vue'
+export default {
+  components: { Header },
+    name: 'game-detail-header',
+    props: {
+        game: {
+            type: Object,
+            required: true
+        },
+        type: {
+            type: String,
+            default: 'normal'
+        },
+        fixed: {
+            type: Boolean,
+            default: false
+        }
+    },
+    computed: {
+        isHeaderNormal() {
+            return this.type === 'normal';
+        },
+        isHeaderSlim() {
+            return this.type === 'slim';
+        },
+        teamA() {
+            return this.game.teams[0]
+        },
+        teamB() {
+            return this.game.teams[1]
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.game-detail-header {
+    display: flex;
+    min-height: 201px;
+    padding-bottom: 26px;
+    padding-top: 10px;
+    
+    &--slim {
+        height: 73px;
+        min-height: 0;
+        padding: 0;
+        align-items: center;
+    }
+
+    &--fixed {
+        position: fixed;
+        width: 100%;
+        background: var(--color-background);
+    }
+
+    &__container {
+        z-index: 2;
+
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        width: 100%;
+    }
+
+    ::v-deep .header {
+        background: transparent;
+        border: none;
+    }
+
+    &__title {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        margin-left: -20px;
+
+        color: #FFFFFF80
+    }
+
+    &__teams {
+        display: flex;
+        justify-content: center;
+        gap: 50px;
+        align-items: flex-start;
+
+        &--slim {
+            gap: 18px;
+            align-items: center;
+        }
+    }
+
+    &__teams--slim .team,
+    &__teams--slim .team__image {
+        height: 30px;
+        width: 30px;
+    }
+
+    &__teams--slim .team__versus {
+        margin-top: 0;
+    }
+} 
+
+.team {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    width: 100px;
+    &__image {
+        width: 50px;
+        height: 50px;
+    }
+
+    &__name {
+        color: var(--color-text);
+        text-align: center;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 16px;
+    }
+
+    &__versus {
+        font-size: 20px;
+        font-weight: 400;
+        margin-top: 16px;
+        color: var(--color-text);
+    }
+}
+
+.background {
+    z-index: 1;
+    position: absolute;
+  
+    width: 100%;
+    height: 201px;
+    overflow: hidden;
+    display: flex;
+    filter: blur(40px);
+    -webkit-filter: blur(40px);
+    background: rgba(255, 255, 255, 0.02);
+    
+    &--slim {
+        height: 73px;
+        top: 0;
+        left: 0;
+        clip-path: inset(0);
+    }
+
+    &__image {
+        width: 50%;
+        height: 100%;
+        background-size: 600%;
+        background-position: center;
+    }
+}
+
+</style>
