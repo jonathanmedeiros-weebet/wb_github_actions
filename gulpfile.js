05dc5cd7d7c2a41ad68f4b0c142e1a179a0c9ab1
Thiago Faustino <thiago.faustino@lti.net.br>
2024-07-04 21:19:53 -0300

    remove

--- gulpfile.js
+++ gulpfile.js
@@ -2144,17 +2144,6 @@ gulp.task('agorabets.com', function (done) {
     });
 });
 
-gulp.task('brasilplay.com', function (done) {
-    tasks(done, {
-        server: "front5.wee.bet",
-        host: "brasilplay.com",
-        banca: "BRASIL PLAY",
-        google_tag_part_1: "<!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TJFN8MJF');</script><!-- End Google Tag Manager -->",
-        google_tag_part_2: "<!-- Google Tag Manager (noscript) --><noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-TJFN8MJF'height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript><!-- End Google Tag Manager (noscript) -->",
-        styles: "",
-    });
-});
-
 gulp.task('fortalezabets.online', function (done) {
     tasks(done, {
         server: "front5.wee.bet",
