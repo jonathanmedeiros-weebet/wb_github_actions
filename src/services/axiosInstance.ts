9ad9e986d41c131d9593e3dc11ae0334effe3113
Jonathan Tifinancias <jonathantifinancias@gmail.com>
2024-07-09 17:04:07 -0300

    feat: axios downgrade

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -2,18 +2,18 @@ import axios from "axios";
 
 export const axiosInstance = () => {
   const axiosInstance = axios.create({
-      baseURL: import.meta.env.VITE_API_URL,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
 
-    axiosInstance.interceptors.response.use(
-      (response: any) => response.data,
-      (error: any) => Promise.reject(error)
-    );
+  axiosInstance.interceptors.response.use(
+    (response: any) => response.data,
+    (error: any) => Promise.reject(error)
+  );
 
-    return axiosInstance;
-  };
+  return axiosInstance;
+};
 
   
