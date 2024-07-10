2a2f2e71489dac12f5b2bbc72c66ce02b7feb81a
Marcos Felipe <marcos.felipe@lti.net.br>
2024-07-10 16:15:37 -0300

    feat(ValidarAposta) css atualizado

--- src/views/TicketsView.vue
+++ src/views/TicketsView.vue
@@ -2,25 +2,6 @@
   <div class="tickets">
     <Header title="Bilhetes" />
   </div>
-  
-  <div class="modal-modalities__items">
-    <span class="modal-modalities__text">
-      Digite o código de aposta para realizar a consulta do bilhete
-    </span>
-    <w-input
-      class="modal-modalities__input"
-      name="consult_ticket"
-      placeholder="Ex.: EA5D-DG41"
-      type="email"
-    />
-    <w-button
-      id="btn-salvar"
-      text="Consultar"
-      value="salvar"
-      name="btn-salvar"
-    />
-  </div>
-  
 </template>
 
 <script>
