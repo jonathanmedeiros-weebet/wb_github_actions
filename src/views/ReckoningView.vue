c5f8ae40f6c882168f0b8ac765fc28883d6ca55f
Marcos Felipe <marcos.felipe@lti.net.br>
2024-07-24 10:35:28 -0300

    feat(Reckoning) calendario

--- src/views/ReckoningView.vue
+++ src/views/ReckoningView.vue
@@ -3,11 +3,18 @@
     <Header 
       :title="title" 
       :showCalendarButton="true" 
-      :showBackButton="true" 
+      :showBackButton="true"
+      @calendarClick="handleOpenCalendarModal"
+      @searchClick="handleOpenSearchModal"
+    />
+    <ModalCalendar
+      v-if="showModalCalendar"
+      @closeModal="handleCloseCalendarModal"
+      @change="handleCalendar"
     />
     <div class="reckoning__container">
       <span class="date">
-        {{ this.startDate }} - {{this.endDate}}
+        {{ startDate }} - {{ endDate }}
         <IconClose class="date__close" />
       </span>
       <div class="balance">
@@ -25,7 +32,7 @@
           <span class="collapse__title">Entradas</span>
           <div class="collapse__value">
             <IconAdd class="collapse__icon" />
-            {{entry}}
+            {{ entry }}
           </div>
         </div>
         <div v-if="collapsedInputs" class="collapse__content">
@@ -57,7 +64,7 @@
           <span class="collapse__title">Saídas</span>
           <div class="collapse__value">
             <IconRemove class="collapse__icon-remove" />
-            <span class="collapse__balance">{{totalExits}}</span>
+            <span class="collapse__balance">{{ totalExits }}</span>
           </div>
         </div>
         <div v-if="collapsedExits" class="collapse__content">
@@ -85,29 +92,29 @@
         </div>
       </div>
       <div class="collapse__line"></div>
-        <div class="credit">
-          <span>Créditos</span>
-          <div class="credit__date">
-            <IconAdd class="credit__icon" />
-            <span class="credit__value">{{credit}}</span>
-          </div>
-        </div>
-        <div class="collapse__line"></div>
-        <div class="debit">
-          <span>Débitos</span>
-          <div class="debit__date">
-            <IconRemove class="debit__icon" />
-            <span class="debit__value">{{debit}}</span>
-          </div>
-        </div>
-        <div class="collapse__line"></div>
-        <div class="balance">
-          <span>Saldo</span>
-          <div class="balance__date">
-            <span class="balance__value">{{balance}}</span>
-          </div>
+      <div class="credit">
+        <span>Créditos</span>
+        <div class="credit__date">
+          <IconAdd class="credit__icon" />
+          <span class="credit__value">{{ credit }}</span>
         </div>
       </div>
+      <div class="collapse__line"></div>
+      <div class="debit">
+        <span>Débitos</span>
+        <div class="debit__date">
+          <IconRemove class="debit__icon" />
+          <span class="debit__value">{{ debit }}</span>
+        </div>
+      </div>
+      <div class="collapse__line"></div>
+      <div class="balance">
+        <span>Saldo</span>
+        <div class="balance__date">
+          <span class="balance__value">{{ balance }}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -122,8 +129,10 @@ import IconArrowUp from '@/components/icons/IconArrowUp.vue'
 import IconRemove from '@/components/icons/IconRemove.vue'
 import { getCalculationValue } from '@/services/reckoning.service'
 import { computed } from 'vue'
+import moment from 'moment';
 import { formatCurrency } from '@/utilities'
-import { dateFormatInMonthAndYear } from '@/utilities'
+import ModalCalendar from './HomeView/parts/ModalCalendar.vue'
+
 export default {
   name: 'reckoning',
   components: {
@@ -133,7 +142,8 @@ export default {
     IconAdd,
     IconArrowDown,
     IconArrowUp,
-    IconRemove
+    IconRemove,
+    ModalCalendar
   },
   props: {
     initCollapsed: {
@@ -144,9 +154,10 @@ export default {
   data() {
     return {
       title: 'Apuração',
+      showModalCalendar: false,
       balanceCalculation: null,
-      startDate: '2024-07-22',
-      endDate: '2024-07-28',
+      startDate: moment().format('YYYY-MM-DD'),
+      endDate: moment().format('YYYY-MM-DD'),
       relatory: '31/05',
       value: '0,00',
       totalBet: null,
@@ -183,9 +194,23 @@ export default {
     handleSelectModalClick() {
       alert('Modal select')
     },
+    handleOpenCalendarModal() {
+      this.showModalCalendar = true;
+    },
+    handleCloseCalendarModal() {
+      this.showModalCalendar = false;
+    },
+    handleCalendar(dateTime) {
+      console.log(dateTime)
+      this.handleCloseCalendarModal()
+    },
+    handleOpenSearchModal() {
+      console.log('Open search modal')
+    },
     toggleCollapse(section, event) {
       console.log(section)
       if (section === 'input') {
+        console.log(this.startDate);
         this.collapsedInputs = !this.collapsedInputs
       } else if (section === 'exit') {
         this.collapsedExits = !this.collapsedExits
@@ -196,18 +221,18 @@ export default {
     },
     async getValue() {
       try {
-        const res = await getCalculationValue(this.startDate,this.endDate)
-        console.log(res);
-        this.withdraw = formatCurrency(Number(res.withdraw ?? 0));
-        this.commission = formatCurrency(Number(res.total_comissao ?? 0));
-        this.award = formatCurrency(Number(res.total_premios ?? 0));
-        this.totalBet = formatCurrency(Number(res.total_apostado ?? 0));
-        this.entry = formatCurrency(Number(res.total_entradas ?? 0));
-        this.rechargesCartao = formatCurrency(Number(res.cartao ?? 0));
-        this.totalExits = formatCurrency(Number(res.total_saidas ?? 0));
-        this.credit = formatCurrency(Number(res.creditos ?? 0));
-        this.debit = formatCurrency(Number(res.debitos ?? 0));
-        this.balance = formatCurrency(Number(res.saldo ?? 0));
+        const res = await getCalculationValue(this.startDate, this.endDate)
+        console.log(res)
+        this.withdraw = formatCurrency(Number(res.withdraw ?? 0))
+        this.commission = formatCurrency(Number(res.total_comissao ?? 0))
+        this.award = formatCurrency(Number(res.total_premios ?? 0))
+        this.totalBet = formatCurrency(Number(res.total_apostado ?? 0))
+        this.entry = formatCurrency(Number(res.total_entradas ?? 0))
+        this.rechargesCartao = formatCurrency(Number(res.cartao ?? 0))
+        this.totalExits = formatCurrency(Number(res.total_saidas ?? 0))
+        this.credit = formatCurrency(Number(res.creditos ?? 0))
+        this.debit = formatCurrency(Number(res.debitos ?? 0))
+        this.balance = formatCurrency(Number(res.saldo ?? 0))
       } catch (error) {
         console.error('Error fetching data:', error)
       }
