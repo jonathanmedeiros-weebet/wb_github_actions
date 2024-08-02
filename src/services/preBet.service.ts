03d3d7621549b19a3ccd17009955412d875e1ed7
Emanuel Aguiar <emanuel_pe@live.com>
2024-08-02 13:59:02 -0300

    feat: adicionada a fun;cão de confirmação da pre bet

--- src/services/preBet.service.ts
+++ src/services/preBet.service.ts
@@ -7,3 +7,10 @@ export const getPreBetByCode = async ( code: string) => {
    
     return await axiosInstance().get(url);
 }
+
+export const confirmPreBet = async ( payload: any) => {
+    const { apiUrl } = useConfigClient();
+    const url = `${apiUrl}/preapostas/esportes`;
+   
+    return await axiosInstance().post(url, payload);
+}
