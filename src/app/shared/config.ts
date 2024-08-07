f96f5596346062c01d103d3bcdf47358d90edf2c
Marcos Felipe <mf@931089@gmail.com>
2024-08-07 14:47:13 -0300

    feat(Resultados/Apostas) calendário funcionando

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,5 +1,5 @@
 
-const _host = '//localhost';
+const _host = '//central.demo.wee.bet';
 const _center = 'https://center7.wee.bet';
 const _live = 'https://streaming.wee.bet';
 const _stats = 'https://stats.wee.bet';
@@ -8,7 +8,7 @@ export const config: any = {
     BANCA_NOME: 'DEMO',
     HOST: _host,
     BASE_URL: `${_host}/api`,
-    LOKI_URL: '//localhost:8000',
+    LOKI_URL: 'https://loki1.weebet.tech',
     SHARED_URL: 'demo.wee.bet',
     CENTER_HOST: _center,
     LIVE_HOST: _live,
@@ -16,7 +16,7 @@ export const config: any = {
     CENTER_API: `${_center}/v1`,
     SPORTS_URL: `${_host}/api/esportes`,
     LOTTERIES_URL: `${_host}/api/loteria`,
-    SLUG: 'weebet.marcos',
+    SLUG: 'demo.wee.bet',
     // LOGO: _host.replace('central.', '')+'/assets/images/logo_banca.png',
     LOGO: 'https://weebet.s3.amazonaws.com/demo.wee.bet/logos/logo_banca.png',
     LOGO_IMPRESSAO: 'https://weebet.s3.amazonaws.com/demo.wee.bet/logos/logo_impressao.png',
