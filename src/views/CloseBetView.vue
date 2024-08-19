e88aa943a2ad812d662a7e21c4826b0b92f622a9
Marcos Felipe <marcos.felipe@lti.net.br>
2024-08-19 13:47:18 -0300

    feat ajustes css

--- src/views/CloseBetView.vue
+++ src/views/CloseBetView.vue
@@ -62,7 +62,9 @@
                 <template v-else-if="betItem.sport === MODALITY_SPORT_E_SPORTS">
                   <IconGame :size="16"/>
                 </template>
-                <span :class="{'gain__strikethrough': newEarningPossibility !== null}">{{ truncateText(betItem.time_a_nome + " x " + betItem.time_b_nome) }}</span>
+                <div class="bet__team-name">
+                  <span :class="{'gain__strikethrough': newEarningPossibility !== null}">{{ truncateText(betItem.time_a_nome + " x " + betItem.time_b_nome) }}</span>
+                </div>
               </span>
               <template v-if="showFinished">
                 <p :class="{ 
@@ -374,7 +376,7 @@ export default {
   &__container {
     display: flex;
     flex-direction: column;
-    gap: 6px;
+    
     margin: 0;
     padding: 0 10px;
     min-height: 100%;
@@ -383,7 +385,7 @@ export default {
   &__ticket {
     display: flex;
     flex-direction: column;
-    gap: 12px;
+    
     padding: 19px 15px;
     width: 100%;
     background: #181818;
@@ -395,7 +397,7 @@ export default {
 .code {
     display: flex;
     flex-direction: column;
-    gap: 4px;
+    
 
     &__text {
         font-size: 14px;
@@ -411,7 +413,7 @@ export default {
 .info {
     display: flex;
     flex-direction: column;
-    gap: 4px;
+    
     margin-bottom: -10px;
 
     &__item {
@@ -427,9 +429,8 @@ export default {
 .gain {
     display: flex;
     flex-direction: column;
-    gap: 4px;
-    margin-bottom: 10px;
-
+    margin-bottom: 15px;
+    margin-top: 8px;
     &__item {
         display: flex;
         justify-content: space-between;
@@ -457,13 +458,12 @@ export default {
     background: var(--color-background);
     border-radius: 4px;
     margin-bottom: -9px;
-    
+
     &__header, &__info, &__text, &__result {
         display: flex;
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
-        gap: 8px;
     }
 
     &__team {
@@ -475,7 +475,10 @@ export default {
         font-size: 14px;
         color: #ffffff;
         color: var(--color-text);
-        gap: 4px;  
+    }
+
+    &__team-name {
+      margin-left: 4px;
     }
 
     &__date, &__text, &__result {
@@ -518,7 +521,6 @@ export default {
 .buttons {
   display: flex;
   justify-content: space-between;
-  gap: 8px;
   padding-top: 25px;
 }
 
