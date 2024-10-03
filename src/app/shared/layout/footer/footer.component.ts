244f11aeec7ff1cf641ce3294e5fdf6ff1082ad9
Marcelo Mesquita <marcelo.mesquita@lti.net.br>
2024-10-03 08:41:52 -0300

    Fix: removing the need for refresh to apply button to top

--- src/app/shared/layout/footer/footer.component.ts
+++ src/app/shared/layout/footer/footer.component.ts
@@ -133,12 +133,14 @@ export class FooterComponent implements OnInit, AfterViewInit {
         this.container.addEventListener('scroll', this.onScroll.bind(this));
         const bilheteContainer = document.querySelector('.bilhete-container') as HTMLElement;
 
-        if (bilheteContainer && !this.isMobile) {
-            const toTopElement = document.querySelector('.toTop') as HTMLElement;
-            if(toTopElement){
-                toTopElement.style.right = `calc(20px + ${bilheteContainer.clientWidth}px)`;
+        setTimeout(() => {
+            if (bilheteContainer && !this.isMobile) {
+                const toTopElement = document.querySelector('.toTop') as HTMLElement;
+                if (toTopElement) {
+                    toTopElement.style.right = `calc(20px + ${bilheteContainer.clientWidth}px)`;
+                }
             }
-        }
+        }, 0);
     }
     
     onScroll() {
