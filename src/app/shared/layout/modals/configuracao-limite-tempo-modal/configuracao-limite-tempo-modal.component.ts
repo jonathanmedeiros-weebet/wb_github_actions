f083f2fdfa06091d0a5aaac478e2fac40e16a7dc
William Felipe <william.felipe@lti.net.br>
2024-10-14 15:41:29 -0300

    feat: create config limit time modal

---
+++ src/app/shared/layout/modals/configuracao-limite-tempo-modal/configuracao-limite-tempo-modal.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from 'src/app/services';
+
+@Component({
+  selector: 'app-configuracao-limite-tempo-modal',
+  templateUrl: './configuracao-limite-tempo-modal.component.html',
+  styleUrls: ['./configuracao-limite-tempo-modal.component.css']
+})
+export class ConfiguracaoLimiteTempoModalComponent implements OnInit {
+  constructor(
+    public activeModal: NgbActiveModal,
+    private auth: AuthService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  back () {
+    this.activeModal.close();
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+}
