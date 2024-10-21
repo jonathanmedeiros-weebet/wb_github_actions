d85cfce1a230cd52b7a4f96740832183b974f9a4
Gustavo <gustavo.andrade@lti.net.br>
2024-10-21 09:06:08 -0300

    fix: Removing pinned tag from GTM at customer request.

--- gulpfile.js
+++ gulpfile.js
@@ -1983,8 +1983,6 @@ gulp.task('bnrbet.com', function (done) {
         host: "bnrbet.com",
         banca: "BNR BET",
         styles: "",
-        google_tag_part_1: "<!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KVT8RKHH');</script><!-- End Google Tag Manager -->",
-        google_tag_part_2: "<!-- Google Tag Manager (noscript) --><noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-KVT8RKHH'height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript><!-- End Google Tag Manager (noscript) -->",
     });
 });
 
