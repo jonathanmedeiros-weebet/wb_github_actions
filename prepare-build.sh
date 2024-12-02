90acea18ff8617c4891c96cf6445dc4859071418
Jonathan Tifinancias <jonathantifinancias@gmail.com>
2024-12-02 15:46:19 -0300

    scrip pre-build

---
+++ prepare-build.sh
@@ -0,0 +1,11 @@
+# gulp build-bet4.wee.bet
+
+flutter pub get
+
+flutter pub run flutter_launcher_icons:main
+
+flutter pub run flutter_native_splash:create
+
+flutter build apk --release
+
+# flutter run --release
