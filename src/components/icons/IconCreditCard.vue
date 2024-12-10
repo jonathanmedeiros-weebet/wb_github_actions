4deffb52ad9084efd3acb2ed5a6cc04164e6c719
Jonathan Tifinancias <jonathantifinancias@gmail.com>
2024-12-10 11:14:52 -0300

    feat: add card menu

---
+++ src/components/icons/IconCreditCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 -960 960 960"
+        :fill="color"
+        :width="size"
+        :height="size"
+    >
+        <path d="M880-720v480q0 33-23.5 56.5T800-160H160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720Zm-720 80h640v-80H160v80Zm0 160v240h640v-240H160Zm0 240v-480 480Z"/>
+    </svg>
+</template>
+
+<script>
+export default {
+    name: 'icon-credit-card',
+    props: {
+        size: {
+            type: Number,
+            default: 20
+        },
+        color: {
+            type: String,
+            default: '#ffffff'
+        }
+    }
+}
+</script>
