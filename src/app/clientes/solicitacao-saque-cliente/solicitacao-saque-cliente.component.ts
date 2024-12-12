468fe250f2f48f021686f521391ae27e04607058
Kassio Gimansk <kassio.gimansk@lti.net.br>
2024-12-12 11:33:06 -0300

    Debug

--- src/app/clientes/solicitacao-saque-cliente/solicitacao-saque-cliente.component.ts
+++ src/app/clientes/solicitacao-saque-cliente/solicitacao-saque-cliente.component.ts
@@ -204,7 +204,7 @@ export class SolicitacaoSaqueClienteComponent extends BaseFormComponent implemen
                         this.faceMatchService.updadeFacematch({ document: this.cliente.cpf, first_withdraw: true }).subscribe()
                         this.faceMatchFirstWithdrawValidated = true;
                     } else {
-                        this.legitimuzService.closeModal();
+                        // this.legitimuzService.closeModal();
                         this.messageService.error(this.translate.instant('face_match.Identity_not_verified'));
                         this.faceMatchFirstWithdrawValidated = false;
                     }
