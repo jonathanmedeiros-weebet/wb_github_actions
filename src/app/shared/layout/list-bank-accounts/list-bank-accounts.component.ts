200d41dff55895519e87c396c468d745ad41c78d
Gustavo <gustavo.andrade@lti.net.br>
2024-12-18 19:13:17 -0300

    add delete bank account

--- src/app/shared/layout/list-bank-accounts/list-bank-accounts.component.ts
+++ src/app/shared/layout/list-bank-accounts/list-bank-accounts.component.ts
@@ -1,8 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AddBankAccountModalComponent } from '../modals/add-bank-account-modal/add-bank-account-modal.component';
-import { ClienteService } from 'src/app/services';
-// import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ClienteService, MessageService, UtilsService } from 'src/app/services';
+import { ConfirmModalComponent } from '../modals';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+import { Bank } from '../../models/bankAccounts/bank';
 
 @Component({
   selector: 'app-list-bank-accounts',
@@ -10,37 +14,75 @@ import { ClienteService } from 'src/app/services';
   styleUrls: ['./list-bank-accounts.component.scss']
 })
 export class ListBankAccountsComponent {
-  @Input() showHeaderMobile: boolean = false;
+    @Input() showHeaderMobile: boolean = false;
+    public accounts: any[] = []
+    unsub$ = new Subject();
+    public banks: Array<Bank> = [];
 
-  public accounts: any[] = []
+    constructor(
+        public activeModal: NgbActiveModal,
+        private modalService: NgbModal,
+        private clienteService: ClienteService,
+        private messageService: MessageService,
+        private translate: TranslateService,
+        private utilsService: UtilsService,
+    ) {}
 
-  constructor(
-    public activeModal: NgbActiveModal,
-    private modalService: NgbModal,
-    private clienteService: ClienteService
-  ) {}
+    ngOnInit() {
+        this.loadPage();
+        this.getBanks();
+    }
 
-  ngOnInit() {
-    this.loadPage();
-  }
+    private loadPage() {
+        this.clienteService
+        .allBankAccounts()
+        .toPromise()
+        .then((allBanks) => this.accounts = allBanks);
+    }
 
-  private loadPage() {
-    this.clienteService
-      .allBankAccounts()
-      .toPromise()
-      .then((allBanks) => this.accounts = allBanks);
-  }
+    public openAddBankAccount() {
+        const modalRef = this.modalService.open(AddBankAccountModalComponent);
+        modalRef.componentInstance.showHeader = this.showHeaderMobile;
 
-  public openAddBankAccount() {
-    const modalRef = this.modalService.open(AddBankAccountModalComponent);
-    modalRef.componentInstance.showHeader = this.showHeaderMobile;
+        modalRef.result.then(({isReload}) => {
+        if(Boolean(isReload)) this.loadPage();
+        });
+    }
 
-    modalRef.result.then(({isReload}) => {
-     if(Boolean(isReload)) this.loadPage();
-    });
-  }
+    public openConfirmModal(accountItem) {
+            const modalConfirm = this.modalService.open(ConfirmModalComponent, { centered: true });
+            modalConfirm.componentInstance.title = 'Excluir Conta Bancária';
+            modalConfirm.componentInstance.msg = 'Tem certeza que deseja excluir a conta bancária ?';
 
-  public toBack() {
-    this.activeModal.dismiss();
-  }
+            modalConfirm.result.then(
+                (result) => {
+                    this.clienteService.deleteBankAccount({ id: accountItem.id})
+                        .pipe(takeUntil(this.unsub$))
+                        .subscribe(
+                            () => {
+                                this.messageService.success(this.translate.instant('geral.successfullyDeleted'))
+                                this.activeModal.close({isReload: true}),
+                                this.loadPage()
+                            },
+                            error => this.handleError(error)
+                        );
+                },
+                (reason) => { }
+            );
+        }
+    
+    handleError(mensagem: string) {
+        this.messageService.error(mensagem);
+    }
+
+    public toBack() {
+        this.activeModal.dismiss();
+    }
+
+    getBanks() {
+        this.utilsService.getBanks().subscribe(
+            banks => this.banks = banks,
+            error => this.handleError(error)
+        );
+    }
 }
