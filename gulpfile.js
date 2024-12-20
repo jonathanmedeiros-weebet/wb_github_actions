e7109ce52305eec96046aef20b26c44dca78d872
Emerson Silva <emerson.silva@wee.bet>
2024-12-20 15:56:36 -0300

    migration: migrating whitout database bet5.wee.bet > totalsorte.com.br

--- gulpfile.js
+++ gulpfile.js
@@ -284,10 +284,10 @@ gulp.task('bet4.wee.bet', function (done) {
     });
 });
 
-gulp.task('bet5.wee.bet', function (done) {
+gulp.task('totalsorte.com.br', function (done) {
     tasks(done, {
         server: "front2.wee.bet",
-        host: "bet5.wee.bet",
+        host: "totalsorte.com.br",
         banca: "Total Sorte",
         styles: ""
     });
