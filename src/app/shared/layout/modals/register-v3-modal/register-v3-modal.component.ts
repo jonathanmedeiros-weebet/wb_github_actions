64dc19a4d43a289452bd692e17ce575eee1b5db5
Gustavo <gustavo.andrade@lti.net.br>
2025-02-19 18:15:54 -0300

    feature: new layout register

---
+++ src/app/shared/layout/modals/register-v3-modal/register-v3-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-v3-modal',
+  templateUrl: './register-v3-modal.component.html',
+  styleUrls: ['./register-v3-modal.component.scss']
+})
+export class RegisterV3ModalComponent {
+
+}
