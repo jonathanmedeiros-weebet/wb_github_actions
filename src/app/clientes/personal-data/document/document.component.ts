63433adad35704bc85ba9463103779ff3e3c6c5a
Iuri Brito <iuri.brito@lti.net.br>
2025-02-21 10:48:21 -0300

    feat: load data in dogs

--- src/app/clientes/personal-data/document/document.component.ts
+++ src/app/clientes/personal-data/document/document.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ClienteService, MessageService } from 'src/app/services';
+
+export interface CustomerResponse {
+    cpf: string;
+    nome: string;
+    sobrenome: string;
+    dataNascimento: string;
+    nationality: string;
+    genero: string;
+}
 
 @Component({
   selector: 'app-document',
   templateUrl: './document.component.html',
   styleUrls: ['./document.component.css']
 })
-export class DocumentComponent {
+export class DocumentComponent implements OnInit {
+    customer: any;
 
+    constructor(
+        private clienteService: ClienteService,
+        private messageService: MessageService,
+        private translate: TranslateService,
+    ) {}
+
+    ngOnInit(): void {
+        this.loadCustomerEmail();
+    }
+
+    loadCustomerEmail() {
+        const user = JSON.parse(localStorage.getItem('user'));
+        this.clienteService.getCliente(user.id)
+            .subscribe({
+                next: (res: CustomerResponse) => {
+                    this.customer = res;
+                },
+                error: () => {
+                    this.messageService.error(this.translate.instant('erroInesperado'));
+                }
+            });
+    }
 }
