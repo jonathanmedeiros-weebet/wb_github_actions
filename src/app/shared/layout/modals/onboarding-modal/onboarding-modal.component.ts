be7b8b6bbc30126c8bb38abea52b3cbb0ebc2eeb
Gustavo <gustavo.andrade@lti.net.br>
2025-02-23 22:11:13 -0300

    feature: new layout register v3

--- src/app/shared/layout/modals/onboarding-modal/onboarding-modal.component.ts
+++ src/app/shared/layout/modals/onboarding-modal/onboarding-modal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RegisterV3ModalComponent } from '../register-v3-modal/register-v3-modal.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-onboarding-modal',
@@ -6,9 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./onboarding-modal.component.scss']
 })
 export class OnboardingModalComponent {
+    public modalRef;
 
+    constructor(
+            private modalService: NgbModal,
+        ) {
+        }
 
-
-  register(){
-  }
+    register(){
+        this.modalRef = this.modalService.open(
+            RegisterV3ModalComponent,
+            {
+                ariaLabelledBy: 'modal-basic-title',
+                size: 'md',
+                centered: true,
+                windowClass: 'modal-750 modal-cadastro-cliente',
+                backdrop: 'static'
+            }
+        );
+    }
 }
