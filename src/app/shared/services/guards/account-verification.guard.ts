2df917d4c9d2cf1bcc9b6aa1c5326d28d16edd08
Jonathan Tifinancias <jonathantifinancias@gmail.com>
2025-02-26 07:53:33 -0300

    fix: adjust account verification guard

--- src/app/shared/services/guards/account-verification.guard.ts
+++ src/app/shared/services/guards/account-verification.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { AccountVerificationService } from '../account-verification.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,8 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class AccountVerificationGuard implements CanActivate {
   constructor(
     private accountVerificationService: AccountVerificationService,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
   canActivate(
@@ -17,23 +19,29 @@ export class AccountVerificationGuard implements CanActivate {
     state: RouterStateSnapshot
   ): boolean | UrlTree {
     void next;
-    const nextUrl = state.url;
-    const previousUrl = window.location.pathname;
 
-    const accountVerified: boolean = this.accountVerificationService.accountVerified.getValue();
-    if (accountVerified) {
+    if(this.authService.isLoggedIn() && this.authService.isCliente()) {
+
+      const nextUrl = state.url;
+      const previousUrl = window.location.pathname;
+
+      const accountVerified: boolean = this.accountVerificationService.accountVerified.getValue();
+      if (accountVerified) {
+        return true;
+      }
+
+      const modalRef = this.accountVerificationService.openModalAccountVerificationAlert();
+      modalRef.result.then((isClosed) => {
+        if (isClosed) {
+          if (previousUrl == nextUrl) {
+            return this.router.navigate(['/']);
+          }
+        } 
+      });
+
+      return false;
+    } else {
       return true;
     }
-
-    const modalRef = this.accountVerificationService.openModalAccountVerificationAlert();
-    modalRef.result.then((isClosed) => {
-      if (isClosed) {
-        if (previousUrl == nextUrl) {
-          return this.router.navigate(['/']);
-        }
-      } 
-    });
-
-    return false;
   }
 }
