8c0f175c4d4e0dd87f0183815fc2c6cfbdaed782
Gustavo <gustavo.andrade@lti.net.br>
2025-03-19 13:50:28 -0300

    refactor: default code playbrokers

--- src/app/shared/utils/pipes/format-payment-method.pipe.ts
+++ src/app/shared/utils/pipes/format-payment-method.pipe.ts
@@ -34,7 +34,7 @@ export class FormatPaymentMethodPipe implements PipeTransform {
             case 'lisboapay':
                 return 'LisboaPAY';
             case 'paybrokers':
-                return 'Paybrokers'
+                return 'Paybrokers';
 		}
 
         return "";
