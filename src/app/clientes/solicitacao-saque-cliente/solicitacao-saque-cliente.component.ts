2e532367586b57110522fce3bcbc36a7c9c43d69
Gustavo <gustavo.andrade@lti.net.br>
2025-03-26 09:51:29 -0300

    fix: to test sandbox

--- src/app/clientes/solicitacao-saque-cliente/solicitacao-saque-cliente.component.ts
+++ src/app/clientes/solicitacao-saque-cliente/solicitacao-saque-cliente.component.ts
@@ -42,7 +42,7 @@ export class SolicitacaoSaqueClienteComponent extends BaseFormComponent implemen
     cliente: Cliente;
     modalRef;
 
-    pspsSaqueAutomatico = ['SAUTOPAY', 'PRIMEPAG', 'PAGFAST', 'BIGPAG', 'LETMEPAY', 'PAAG', 'PAY2M', 'OKTO', 'PIXS', 'BIGPAGV3'];
+    pspsSaqueAutomatico = ['SAUTOPAY', 'PRIMEPAG', 'PAGFAST', 'BIGPAG', 'LETMEPAY', 'PAAG', 'PAY2M', 'OKTO', 'PIXS', 'BIGPAGV3', 'PAYBROKERS'];
     respostaSolicitacao;
 
     rotaCompletarCadastro: string;
