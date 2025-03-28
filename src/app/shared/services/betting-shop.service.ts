f20683063d31f8720507f74809a96328632bf261
William Felipe <william.felipe@lti.net.br>
2025-03-28 13:45:07 -0300

    adjust: sending request options in request

--- src/app/shared/services/betting-shop.service.ts
+++ src/app/shared/services/betting-shop.service.ts
@@ -4,23 +4,25 @@ import { catchError, map, take } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { ErrorService } from './utils/error.service';
 import { config } from '../config';
+import { HeadersService } from './utils/headers.service';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class BettingShopService {
-    private bettingShopUrl = `${config.LOKI_URL}/betting-shop`;
+    private bettingShopUrl = `${config.LOKI_URL}/betting-shop,`;
 
     constructor(
         private http: HttpClient,
-        private errorService: ErrorService
+        private errorService: ErrorService,
+        private headerService: HeadersService
     ) { }
 
     getBettingShop(id: number): Observable<any> {
         const url = `${this.bettingShopUrl}/${id}`;
     
-        return this.http.get<any>(url)
+        return this.http.get<any>(url, this.headerService.getRequestOptions(true))
             .pipe(
                 map((res) => res.results),
                 catchError((error) => {
