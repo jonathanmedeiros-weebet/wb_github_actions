c78d753e20752f741ade7d92e96757dd412b10e6
Kassio Gimansk <kassio.gimansk@lti.net.br>
2025-03-31 18:39:08 -0300

    Fix

--- gulpfile.js
+++ gulpfile.js
@@ -1076,10 +1076,11 @@ gulp.task('treinamento.wee.bet', function (done) {
     tasks(done, {
         server: "front3.weebet.tech",
         host: "treinamento.wee.bet",
-        banca: "Treinamento",
-        styles: "", 
+        banca: "Treinamento Weebet",
+        styles: "",
         google_tag_part_1: "<!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N5PB557T');</script><!-- End Google Tag Manager -->",
         google_tag_part_2: "<!-- Google Tag Manager (noscript) --><noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-N5PB557T'height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript><!-- End Google Tag Manager (noscript) -->"
+
     });
 });
 
