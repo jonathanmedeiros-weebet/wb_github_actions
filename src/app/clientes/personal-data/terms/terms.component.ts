1f23fe1c8899d02fd551508ce5fe6cbaa41838f8
weydson <weydson.brito@lti.net.br>
2025-04-01 14:10:02 -0300

    feat: implement dynamic policy links using slug

--- src/app/clientes/personal-data/terms/terms.component.ts
+++ src/app/clientes/personal-data/terms/terms.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import {config} from '../../../shared/config';
 
 @Component({
   selector: 'app-terms',
   templateUrl: './terms.component.html',
   styleUrls: ['./terms.component.css']
 })
-export class TermsComponent {}
+export class TermsComponent {
+
+    get termsAndConditionsLink(): string[] {
+        return [`/${config.SLUG}/informacoes/termos-condicoes`];
+    }
+
+    get privacyPolicyLink(): string[] {
+        return [`/${config.SLUG}/informacoes/politica-privacidade`];
+    }
+
+    get termsOfServiceLink(): string[] {
+        return [`/${config.SLUG}/informacoes/termos-servico`];
+    }
+
+}
