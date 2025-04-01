7105f482a7fcb841c14f0af8c3b1308657726db6
Jonathan Medeiros <jonathan.medeiros@lti.net.br>
2025-04-01 14:40:41 -0300

    fix: adjust components type in personal-data page

--- src/app/clientes/personal-data/terms/terms.component.ts
+++ src/app/clientes/personal-data/terms/terms.component.ts
@@ -7,17 +7,10 @@ import {config} from '../../../shared/config';
   styleUrls: ['./terms.component.css']
 })
 export class TermsComponent {
-
-    get termsAndConditionsLink(): string[] {
-        return [`/${config.SLUG}/informacoes/termos-condicoes`];
-    }
-
-    get privacyPolicyLink(): string[] {
-        return [`/${config.SLUG}/informacoes/politica-privacidade`];
-    }
-
-    get termsOfServiceLink(): string[] {
-        return [`/${config.SLUG}/informacoes/termos-servico`];
-    }
-
+    public termsAndConditionsLink: string[] = [`/${config.SLUG}/informacoes/termos-condicoes`];
+    public privacyPolicyLink: string[] = [`/${config.SLUG}/informacoes/politica-privacidade`];
+    public termsOfServiceLink: string[] = [`/${config.SLUG}/informacoes/termos-servico`];
+    public showTermsAndConditions: boolean = true;
+    public showPrivacyPolicy: boolean = true;
+    public showTermsOfService: boolean = false;
 }
