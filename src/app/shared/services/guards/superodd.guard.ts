51316c646de578310365dcc34caa4156bbbd6987
Iuri Brito <iuri.brito@lti.net.br>
2025-04-09 11:16:09 -0300

    feat: remove comments

--- src/app/shared/services/guards/superodd.guard.ts
+++ src/app/shared/services/guards/superodd.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-// Import our authentication service
 import { AuthService } from '../auth/auth.service';
 
 @Injectable({
@@ -11,7 +10,6 @@ export class SuperoddGuard implements CanActivate {
     constructor(private auth: AuthService, private router: Router) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        // If user is not logged in we'll send them to the homepage
         if (!this.auth.modalidadeHabilitada('superodd')) {
             this.router.navigate(['/']);
             return false;
