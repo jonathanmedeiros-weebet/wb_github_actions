d14a6d62ebb8e463433d1ff9dd1c89ff6acd4755
Kassio Gimansk <kassio.gimansk@lti.net.br>
2025-05-08 09:57:15 -0300

    Feat: Added Generic information steps component

--- src/app/shared/layout/modals/validation/generic-information-steps/generic-information-steps.component.ts
+++ src/app/shared/layout/modals/validation/generic-information-steps/generic-information-steps.component.ts
@@ -1,6 +1,4 @@
 import { AfterViewInit, Component, Input, QueryList, Type, ViewChildren, ViewContainerRef } from '@angular/core';
-import { IconAssuredWorkloadComponent } from '../../../icons/icon-assured-workload';
-import { IconVerifiedUserComponent } from '../../../icons/icon-verified-user';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 
@@ -12,28 +10,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './generic-information-steps.component.scss'
 })
 export class GenericInformationStepsComponent implements AfterViewInit {
-
-// @Input() data: any; 
-
-//   public infoData :any = [
-
-//     {
-//       title: 'Jogando com a regra',
-//       items: [
-//         {
-//           text: 'Como cumprimento da legislação, precisamos realizar essa etapa de validação',
-//           icon: IconAssuredWorkloadComponent,
-//         },
-//         {
-//           text: 'Essa medida garante mais segurança para você em nossa plataforma!',
-//           icon: IconVerifiedUserComponent,
-//         }
-//       ]
-//     }
-    
-//   ]
-
-
+ @Input() textButton: string;
   @Input() sections: {
     title: string;
     items: {
@@ -44,22 +21,24 @@ export class GenericInformationStepsComponent implements AfterViewInit {
   }[] = [];
   @ViewChildren('iconContainer', { read: ViewContainerRef })
   iconContainers!: QueryList<ViewContainerRef>;
-  constructor(private modalService: NgbModal,
+
+  constructor(
+    private modalService: NgbModal,
     public activeModal: NgbActiveModal,
   ) { }
+
+  get hasButton(): boolean {
+    return Boolean(this.textButton);
+  }
 
   ngAfterViewInit() {
     let i = 0;
     for (const section of this.sections) {
-      console.log(section);
       for (const item of section.items) {
-        console.log(item);
         const container = this.iconContainers.get(i++);
         if (container) {
-          console.log(container);
           const compRef = container.createComponent(item.icon);
           if (item.iconProps) {
-            console.log(item.iconProps);
             Object.assign(compRef.instance, item.iconProps);
           }
         }
