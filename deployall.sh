7a6c0bcd117da48a491ea1c2f9b59b6e57a10900
Jonathan Medeiros <jonathan.medeiros@lti.net.br>
2025-08-14 10:48:37 -0300

    chore: ipdate tenant list

--- deployall.sh
+++ deployall.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # String com os clientes separados por espaço
-CLIENTES="26sports.bet amabet.bet amazonsport.io apostacerta.bet apostefacil.bet \
+CLIENTES="26sports.bet amazonsport.io apostacerta.bet apostefacil.bet \
 apostenasorte.bet bet1000.net.br betagora.io betcapital.com betfortuna.com.br \
 betmasteroficial.net betnordeste.net.br betpix77.io betsnordeste.bet betsplay.club \
 betstar.vip bnrbet.com boaaposta.net brasavipcassino.net brinksbet.com copasbet.bet \
-cr7bet.online decolabet.com dentrodaaposta.com deubet.io easybets.club esportbets.bet \
-esporte.fit fadadopirao.bet fizabet.com fortunaplay.club gramadobet.net.br hotbetz.bet \
-ingamedasorte.com joguefacil.bet kwbet.net maraplay.bet megasports.bet miamipix.net \
+cr7bet.online decolabet.com dentrodaaposta.com easybets.club esportbets.bet \
+esporte.fit fadadopirao.bet fizabet.com gramadobet.net.br hotbetz.bet \
+ingamedasorte.com joguefacil.bet kwbet.net maraplay.bet megasports.bet \
 mjrsports.bet moneybets.net newbet.one novabet.site pinbet.bet pinplay.bet pintou.bet \
 playnabet.com primoos.bet reidasorte.bet rondobet.com sortebet777.com sortegol.com \
 sporte.vip sts13.vip supertop3.bet topbets.me ultrabetss.bet vaibet360.com viabet.com.br \
-vitoriasports.vip wingobet.bet starkbet.casino apostecerto.io betvem.io avante.bet"
+vitoriasports.vip wingobet.bet starkbet.casino avante.bet faithbets.bet"
 
 # Limpa logs anteriores
 > build_falhos.txt
