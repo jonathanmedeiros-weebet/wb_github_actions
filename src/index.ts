1fe88d62332e5b3b9346a5ae2ed575f6a98f4e87
Jonathan Medeiros <jonathan.medeiros@lti.net.br>
2025-08-17 23:46:29 -0300

    feat: export tenant hook

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import type { App } from 'vue-demi'
 import Button from '@/components/Button/Button.vue';
 import Navbar from '@/components/Navbar/Navbar.vue';
-import Footer from './components/Footer/Footer.vue';
+import Footer from '@/components/Footer/Footer.vue';
+import { setProvideTenantConfig, useTenantConfig} from '@/composables/useTenantConfig/useTenantConfig';
 
 export default {
   install(app: App) {
@@ -10,3 +11,5 @@ export default {
     app.component('WFooter', Footer);
   }
 };
+
+export { setProvideTenantConfig, useTenantConfig }
