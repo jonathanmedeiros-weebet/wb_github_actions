86f14c62bd358007a95776d08ee18af72cc565d3
Jonathan Medeiros <jonathan.medeiros@lti.net.br>
2025-08-22 15:56:03 -0300

    teste

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
   ssr: false,
+  app: {
+    baseURL: '/',
+  },
   css: [
     "~/assets/styles/main.scss"
   ],
